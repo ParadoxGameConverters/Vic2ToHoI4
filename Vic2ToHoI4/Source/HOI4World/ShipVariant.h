779a3455b4381b09a6118b60264e54003ca23d9e
Idhrendur <idhrendur@hotmail.com>
2019-03-28 21:42:21 -0700

    Add ship variants.

---
+++ Vic2ToHoI4/Source/HOI4World/ShipVariant.h
@@ -0,0 +1,77 @@
+/*Copyright (c) 2019 The Paradox Game Converters Project
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+
+
+#ifndef SHIP_VARIANT_H
+#define SHIP_VARIANT_H
+
+
+
+#include "newParser.h"
+#include "ShipModules.h"
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+
+
+
+namespace HoI4
+{
+	
+class shipVariant: commonItems::parser
+{
+	public:
+		shipVariant(std::istream& theStream);
+
+		shipVariant() = delete;
+		~shipVariant() = default;
+		shipVariant(const shipVariant&) = default;
+		shipVariant(shipVariant&&) = default;
+		shipVariant& operator=(const shipVariant&) = default;
+		shipVariant& operator=(shipVariant&&) = default;
+
+		void setOwningCountryTag(const std::string& tag);
+
+		bool isValidVariant(std::set<std::string> ownedTechnologies);
+
+		friend std::ostream& operator << (std::ostream& output, shipVariant& theVariant);
+
+	private:
+		std::string name;
+		std::string type;
+		std::string owningCountryTag;
+		std::string nameGroup;
+		std::unique_ptr<shipModules> modules;
+		bool obsolete = false;
+
+		std::string ownerTag;
+
+		std::set<std::string> requiredTechnologies;
+		std::set<std::string> blockingTechnologies;
+};
+
+}
+
+
+
+#endif // SHIP_VARIANT_H
---
+++ Vic2ToHoI4Tests/HoI4WorldTests/ShipVariantTests.cpp
@@ -0,0 +1,307 @@
+/*Copyright (c) 2019 The Paradox Game Converters Project
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+
+
+#include "gtest/gtest.h"
+#include "../../Vic2ToHoI4/Source/HOI4World/ShipVariant.h"
+#include <sstream>
+
+
+
+TEST(HoI4World_shipVariantTests, noInputGivesDefaultOutput)
+{
+	std::stringstream input;
+	input << "";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"\"\n";
+	expectedOutput << "\t\ttype = \n";
+	expectedOutput << "\t\tname_group = _\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, nameCanBeInput)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tname = \"ship_name\"\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"ship_name\"\n";
+	expectedOutput << "\t\ttype = \n";
+	expectedOutput << "\t\tname_group = _\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, typeCanBeInput)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\ttype = \"ship_type\"\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"\"\n";
+	expectedOutput << "\t\ttype = ship_type\n";
+	expectedOutput << "\t\tname_group = _\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, nameGroupCanBeInput)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tname_group = NAME_GROUP\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"\"\n";
+	expectedOutput << "\t\ttype = \n";
+	expectedOutput << "\t\tname_group = _NAME_GROUP\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, owningCountryTagCanBeInput)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tname_group = NAME_GROUP\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+	theShipVariant.setOwningCountryTag("TAG");
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"\"\n";
+	expectedOutput << "\t\ttype = \n";
+	expectedOutput << "\t\tname_group = TAG_NAME_GROUP\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, modulesCanBeInput)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tmodules = {\n";
+	input << "\t\tmoduleSpot1 = module1\n";
+	input << "\t\tmoduleSpot2 = module2\n";
+	input << "\t}";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"\"\n";
+	expectedOutput << "\t\ttype = \n";
+	expectedOutput << "\t\tname_group = _\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t\tmodules = {\n";
+	expectedOutput << "\t\t\tmoduleSpot1 = module1\n";
+	expectedOutput << "\t\t\tmoduleSpot2 = module2\n";
+	expectedOutput << "\t\t}\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, canBeSetObsolete)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tobsolete = yes\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"\"\n";
+	expectedOutput << "\t\ttype = \n";
+	expectedOutput << "\t\tname_group = _\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t\tobsolete = yes\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, onlySetObsoleteByYes)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tobsolete = no\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::stringstream output;
+	output << theShipVariant;
+
+	std::stringstream expectedOutput;
+	expectedOutput << "\tcreate_equipment_variant = {\n";
+	expectedOutput << "\t\tname = \"\"\n";
+	expectedOutput << "\t\ttype = \n";
+	expectedOutput << "\t\tname_group = _\n";
+	expectedOutput << "\t\tparent_version = 0\n";
+	expectedOutput << "\t}\n";
+	ASSERT_EQ(expectedOutput.str(), output.str());
+}
+
+
+TEST(HoI4World_shipVariantTests, noRequiredTechsMeansVariantIsValid)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::set<std::string> countrytechs;
+
+	ASSERT_TRUE(theShipVariant.isValidVariant(countrytechs));
+}
+
+
+TEST(HoI4World_shipVariantTests, missingRequiredTechMeansVariantIsInvalid)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\trequired_techs = {\n";
+	input << "\t\trequired_tech1\n";
+	input << "\t}\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::set<std::string> countrytechs;
+
+	ASSERT_FALSE(theShipVariant.isValidVariant(countrytechs));
+}
+
+
+TEST(HoI4World_shipVariantTests, havingRequiredTechsMeansVariantIsValid)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\trequired_techs = {\n";
+	input << "\t\trequired_tech1\n";
+	input << "\t}\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::set<std::string> countryTechs;
+	countryTechs.insert("required_tech1");
+
+	ASSERT_TRUE(theShipVariant.isValidVariant(countryTechs));
+}
+
+
+TEST(HoI4World_shipVariantTests, havingOnlySomeRequiredTechsMeansVariantIsInvalid)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\trequired_techs = {\n";
+	input << "\t\trequired_tech1\n";
+	input << "\t\trequired_tech2\n";
+	input << "\t}\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::set<std::string> countryTechs;
+	countryTechs.insert("required_tech1");
+
+	ASSERT_FALSE(theShipVariant.isValidVariant(countryTechs));
+}
+
+
+TEST(HoI4World_shipVariantTests, havingBlockingTechsMeansVariantIsInvalid)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tblocking_techs = {\n";
+	input << "\t\trequired_tech1\n";
+	input << "\t}\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::set<std::string> countryTechs;
+	countryTechs.insert("required_tech1");
+
+	ASSERT_FALSE(theShipVariant.isValidVariant(countryTechs));
+}
+
+
+TEST(HoI4World_shipVariantTests, havingNoBlockingTechsMeansVariantIsValid)
+{
+	std::stringstream input;
+	input << "ship_type = {\n";
+	input << "\tblocking_techs = {\n";
+	input << "\t\trequired_tech1\n";
+	input << "\t}\n";
+	input << "}";
+	HoI4::shipVariant theShipVariant(input);
+
+	std::set<std::string> countryTechs;
+
+	ASSERT_TRUE(theShipVariant.isValidVariant(countryTechs));
+}
