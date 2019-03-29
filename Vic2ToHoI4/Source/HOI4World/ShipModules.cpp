11e44526d2e7184f82bc47ce4baa50daa6499bef
Idhrendur <idhrendur@hotmail.com>
2019-03-28 21:41:12 -0700

    Add ship modules section of ship variants.

---
+++ Vic2ToHoI4/Source/HOI4World/ShipModules.cpp
@@ -0,0 +1,50 @@
+/*Copyright (c) 2019 The Paradox Game Converters Project
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+
+
+#include "ShipModules.h"
+#include "ParserHelpers.h"
+
+
+
+HoI4::shipModules::shipModules(std::istream& theStream)
+{
+	registerKeyword(std::regex("[a-zA-Z0-9_]+"), [this](const std::string& moduleSlot, std::istream& theStream){
+		commonItems::singleString module(theStream);
+		modules.insert(std::make_pair(moduleSlot, module.getString()));
+	});
+
+	parseStream(theStream);
+}
+
+
+std::ostream& HoI4::operator<<(std::ostream& output, const HoI4::shipModules& theModules)
+{
+	output << "\t\tmodules = {\n";
+	for (auto module: theModules.modules)
+	{
+		output << "\t\t\t" << module.first << " = " << module.second << "\n";
+	}
+	output << "\t\t}\n";
+
+	return output;
+}
