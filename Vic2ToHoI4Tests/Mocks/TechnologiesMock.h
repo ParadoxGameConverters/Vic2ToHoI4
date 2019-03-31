5028806a6f6a1b13d3227d9737cfb4c42a40394f
Idhrendur <idhrendur@hotmail.com>
2019-03-30 17:20:16 -0700

    Update ship variant to use the technologies class.

--- Vic2ToHoI4Tests/Mocks/TechnologiesMock.h
+++ Vic2ToHoI4Tests/Mocks/TechnologiesMock.h
@@ -21,57 +21,13 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 
 
-#ifndef SHIP_VARIANT_H
-#define SHIP_VARIANT_H
+#include "gmock/gmock.h"
+#include "../../Vic2ToHoI4/Source/HOI4World/Technologies.h"
 
 
 
-#include "newParser.h"
-#include "ShipModules.h"
-#include "Technologies.h"
-#include <memory>
-#include <set>
-#include <string>
-
-
-
-namespace HoI4
-{
-	
-class shipVariant: commonItems::parser
+class mockTechnologies: public HoI4::technologies
 {
 	public:
-		shipVariant(std::istream& theStream);
-
-		shipVariant() = delete;
-		~shipVariant() = default;
-		shipVariant(const shipVariant& source);
-		shipVariant(shipVariant&&) = default;
-		shipVariant& operator=(const shipVariant&) = default;
-		shipVariant& operator=(shipVariant&&) = default;
-
-		void setOwningCountryTag(const std::string& tag);
-
-		bool isValidVariant(const technologies& ownedTechs) const;
-
-		friend std::ostream& operator << (std::ostream& output, shipVariant& theVariant);
-
-	private:
-		std::string name;
-		std::string type;
-		std::string owningCountryTag;
-		std::string nameGroup;
-		std::unique_ptr<shipModules> modules;
-		bool obsolete = false;
-
-		std::string ownerTag;
-
-		std::set<std::string> requiredTechnologies;
-		std::set<std::string> blockingTechnologies;
+		MOCK_CONST_METHOD1(hasTechnology, bool(std::string));
 };
-
-}
-
-
-
-#endif // SHIP_VARIANT_H
