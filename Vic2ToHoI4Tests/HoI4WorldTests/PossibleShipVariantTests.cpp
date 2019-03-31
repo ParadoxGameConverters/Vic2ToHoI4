2c57cf06af35576f2bfe2c2d6afe025fe6cd4185
Idhrendur <idhrendur@hotmail.com>
2019-03-30 21:45:17 -0700

    Fix test build

--- Vic2ToHoI4Tests/HoI4WorldTests/PossibleShipVariantTests.cpp
+++ Vic2ToHoI4Tests/HoI4WorldTests/PossibleShipVariantTests.cpp
@@ -22,7 +22,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 
 #include "gtest/gtest.h"
-#include "../../Vic2ToHoI4/Source/HOI4World/PossibleShipVariants.h"
+#include "../../Vic2ToHoI4/Source/HOI4World/ShipTypes/PossibleShipVariants.h"
 #include <sstream>
 
 
