fa104595331e5a54d35ebd8fcddd05669a5e9c36
Idhrendur <idhrendur@hotmail.com>
2019-07-14 10:16:37 -0700

    Move IdeologicalDecisions into a renamed class and its own file.

---
+++ Vic2ToHoI4Tests/HoI4WorldTests/IdeologicalDecisionsTests.cpp
@@ -0,0 +1,51 @@
+/*Copyright (c) 2019 The Paradox Game Converters Project
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+
+
+#include "gtest/gtest.h"
+#include "../Vic2ToHoI4/Source/HOI4World/Decisions/IdeologicalDecisions.h"
+#include <sstream>
+
+
+
+TEST(HoI4World_IdeologicalDecisionsTests, defaultsToEmptyList)
+{
+	std::stringstream input;
+	input << "= {\n";
+	input << "}";
+	HoI4::IdeologicalDecisions ideologicalDecisions(input);
+
+	ASSERT_EQ(ideologicalDecisions.takeCategories().size(), 0);
+}
+
+
+TEST(HoI4World_IdeologicalDecisionsTests, categoriesCanBeInput)
+{
+	std::stringstream input;
+	input << "= {\n";
+	input << "\tcategoryOne = {}\n";
+	input << "\tcategoryTwo = {}\n";
+	input << "}";
+	HoI4::IdeologicalDecisions ideologicalDecisions(input);
+
+	ASSERT_EQ(ideologicalDecisions.takeCategories().size(), 2);
+}
