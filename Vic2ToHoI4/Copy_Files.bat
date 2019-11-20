e55888d1b947802bda0cd6c533b8b37cbe87af84
Idhrendur <idhrendur@hotmail.com>
2019-11-19 18:17:59 -0800

    Make Visual Studio copy flags instead of a script.

--- Vic2ToHoI4/Copy_Files.bat
+++ Vic2ToHoI4/Copy_Files.bat
@@ -2,8 +2,3 @@ rem **Create blankMod**
 del "..\Release\Vic2ToHoI4\blankMod" /Q /S /F
 rmdir "..\Release\Vic2ToHoI4\blankMod" /Q /S
 xcopy "Data_Files\blankMod" "..\Release\Vic2ToHoI4\blankmod" /Y /E /I
-
-rem **Copy flags**
-del "..\Release\Vic2ToHoI4\flags" /Q /S /F
-rmdir "..\Release\Vic2ToHoI4\flags" /Q /S
-xcopy "Data_Files\flags" "..\Release\Vic2ToHoI4\flags" /Y /E /I
