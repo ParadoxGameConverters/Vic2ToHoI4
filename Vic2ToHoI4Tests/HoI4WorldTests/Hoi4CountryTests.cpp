d488cd2eea584795ea9d1d0d1810024c1ced131f
Idhrendur <idhrendur@hotmail.com>
2019-11-29 13:37:30 -0800

    Add tests for filenames

--- Vic2ToHoI4Tests/HoI4WorldTests/Hoi4CountryTests.cpp
+++ Vic2ToHoI4Tests/HoI4WorldTests/Hoi4CountryTests.cpp
@@ -31,7 +31,7 @@ HoI4World_HoI4CountryTests::HoI4World_HoI4CountryTests()
 
 TEST_F(HoI4World_HoI4CountryTests, tagCanBeAssigned)
 {
-	EXPECT_CALL(sourceCountry, getName("english")).WillOnce(testing::Return(""));
+	EXPECT_CALL(sourceCountry, getName("english")).WillOnce(testing::Return(std::nullopt));
 
 	HoI4::Country theCountry(
 		"TAG",
@@ -42,4 +42,75 @@ TEST_F(HoI4World_HoI4CountryTests, tagCanBeAssigned)
 		*theflagsToIdeasMapper
 	);
 	ASSERT_EQ(theCountry.getTag(), "TAG");
+}
+
+
+TEST_F(HoI4World_HoI4CountryTests, filenamesDefaultToTag)
+{
+	EXPECT_CALL(sourceCountry, getName("english")).WillOnce(testing::Return(std::nullopt));
+
+	HoI4::Country theCountry(
+		"TAG",
+		&sourceCountry,
+		theNamesMapper,
+		theGraphicsMapper,
+		theCountryMapper,
+		*theflagsToIdeasMapper
+	);
+	ASSERT_EQ(theCountry.getFilename(), "TAG.txt");
+	ASSERT_EQ(theCountry.getCommonCountryFile(), "TAG.txt");
+}
+
+
+TEST_F(HoI4World_HoI4CountryTests, filenamesBasedOnSourceCountryName)
+{
+	EXPECT_CALL(sourceCountry, getName("english")).WillOnce(testing::Return("source country name"));
+
+	HoI4::Country theCountry(
+		"TAG",
+		&sourceCountry,
+		theNamesMapper,
+		theGraphicsMapper,
+		theCountryMapper,
+		*theflagsToIdeasMapper
+	);
+	ASSERT_EQ(theCountry.getFilename(), "TAG - source country name.txt");
+	ASSERT_EQ(theCountry.getCommonCountryFile(), "source country name.txt");
+}
+
+
+TEST_F(HoI4World_HoI4CountryTests, filenamesReplaceBadCharacters)
+{
+	EXPECT_CALL(sourceCountry, getName("english")).WillOnce(testing::Return("hardname|><"));
+
+	HoI4::Country theCountry(
+		"TAG",
+		&sourceCountry,
+		theNamesMapper,
+		theGraphicsMapper,
+		theCountryMapper,
+		*theflagsToIdeasMapper
+	);
+	ASSERT_EQ(theCountry.getFilename(), "TAG - hardname.txt");
+	ASSERT_EQ(theCountry.getCommonCountryFile(), "hardname.txt");
+}
+
+
+TEST_F(HoI4World_HoI4CountryTests, filenamesConvertFrom1252ToUtf8)
+{
+	EXPECT_CALL(sourceCountry, getName("english")).WillOnce(testing::Return("1252Ç"));
+
+	HoI4::Country theCountry(
+		"TAG",
+		&sourceCountry,
+		theNamesMapper,
+		theGraphicsMapper,
+		theCountryMapper,
+		*theflagsToIdeasMapper
+	);
+
+	std::string filename = theCountry.getFilename();
+	ASSERT_EQ(filename, "TAG - 1252\xC3\x87.txt");
+	std::string commonCountryFilename = theCountry.getCommonCountryFile();
+	ASSERT_EQ(commonCountryFilename, "1252\xC3\x87.txt");
 }
