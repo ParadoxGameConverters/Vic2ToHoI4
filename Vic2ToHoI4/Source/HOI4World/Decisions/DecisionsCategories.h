fc4bd9700320fecbb5b1700d3f09276906707261
Idhrendur <idhrendur@hotmail.com>
2019-12-08 22:37:06 -0800

    Clean up DecisionsCategories based on Resharper suggestions

--- Vic2ToHoI4/Source/HOI4World/Decisions/DecisionsCategories.h
+++ Vic2ToHoI4/Source/HOI4World/Decisions/DecisionsCategories.h
@@ -1,26 +1,3 @@
-/*Copyright (c) 2019 The Paradox Game Converters Project	
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
 #ifndef HOI4_DECISIONS_CATEGORIES_H
 #define HOI4_DECISIONS_CATEGORIES_H
 
@@ -38,7 +15,10 @@ namespace HoI4
 class DecisionsCategories: commonItems::parser
 {
 	public:
-		DecisionsCategories(const std::set<std::string>& _majorIdeologies): majorIdeologies(_majorIdeologies) {};
+		explicit DecisionsCategories(
+			std::set<std::string> _majorIdeologies
+		):	majorIdeologies(std::move(_majorIdeologies)) {}
+
 		friend std::ostream& operator<<(std::ostream& out, const DecisionsCategories& theCategories);
 
 	private:
