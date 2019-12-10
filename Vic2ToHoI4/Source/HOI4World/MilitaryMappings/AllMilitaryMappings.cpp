bd0d940aefaa9d87177c4994cab1c9e26c8cede0
Idhrendur <idhrendur@hotmail.com>
2019-12-09 19:51:54 -0800

    Clean up AllMilitarymappings from Resharper suggestions

--- Vic2ToHoI4/Source/HOI4World/MilitaryMappings/AllMilitaryMappings.cpp
+++ Vic2ToHoI4/Source/HOI4World/MilitaryMappings/AllMilitaryMappings.cpp
@@ -1,26 +1,3 @@
-/*Copyright (c) 2019 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
 #include "AllMilitaryMappings.h"
 #include <fstream>
 
@@ -40,7 +17,7 @@ HoI4::allMilitaryMappings::allMilitaryMappings(std::istream& theStream)
 
 const HoI4::militaryMappings& HoI4::allMilitaryMappings::getMilitaryMappings(const std::vector<std::string>& Vic2Mods) const
 {
-	for (auto mod: Vic2Mods)
+	for (const auto& mod: Vic2Mods)
 	{
 		auto mapping = theMappings.find(mod);
 		if (mapping != theMappings.end())
@@ -63,6 +40,6 @@ HoI4::militaryMappingsFile::militaryMappingsFile()
 	}
 	else
 	{
-		throw("Could not open unit_mappings.txt");
+		throw std::runtime_error{ "Could not open unit_mappings.txt" };
 	}
 }
