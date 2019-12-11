86e584b6b7d9abf8369af7573169fac9221ef1b9
Idhrendur <idhrendur@hotmail.com>
2019-12-10 20:31:41 -0800

    Clean up unit mapping

--- Vic2ToHoI4/Source/HOI4World/MilitaryMappings/UnitMapping.h
+++ Vic2ToHoI4/Source/HOI4World/MilitaryMappings/UnitMapping.h
@@ -1,28 +1,5 @@
-/*Copyright (c) 2019 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
-#ifndef UNIT_MAPPING_H_
-#define UNIT_MAPPING_H_
+#ifndef UNIT_MAPPING_H
+#define UNIT_MAPPING_H
 
 
 
@@ -38,9 +15,9 @@ namespace HoI4
 class UnitMapping: commonItems::parser
 {
 	public:
-		UnitMapping(std::istream& theStream);
+		explicit UnitMapping(std::istream& theStream);
 
-		std::optional<std::pair<std::string, HoI4UnitType>> getMappings() const;
+		[[nodiscard]] std::optional<std::pair<std::string, HoI4UnitType>> getMappings() const;
 
 	private:
 		std::optional<std::string> Vic2Type;
@@ -51,4 +28,4 @@ class UnitMapping: commonItems::parser
 
 
 
-#endif // UNIT_MAPPING_H_
+#endif // UNIT_MAPPING_H
