a61997bc5cd07ea90481565df1ad786a97d1baaa
Idhrendur <idhrendur@hotmail.com>
2019-12-16 19:31:52 -0800

    Clean up LegacyShip.h/cpp

--- Vic2ToHoI4/Source/HOI4World/Navies/LegacyShip.h
+++ Vic2ToHoI4/Source/HOI4World/Navies/LegacyShip.h
@@ -1,28 +1,5 @@
-/*Copyright (c) 2019 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
-#ifndef LEGACY_SHIP_H_
-#define LEGACY_SHIP_H_
+#ifndef LEGACY_SHIP_H
+#define LEGACY_SHIP_H
 
 
 #include "Ship.h"
@@ -34,21 +11,15 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 namespace HoI4
 {
 
-class LegacyShip: public Ship
+class LegacyShip final: public Ship
 {
 	public:
 		LegacyShip(
-			const std::string& _name,
-			const std::string& _type,
-			const std::string& _equipment,
-			const std::string& _owner
-		);
-		LegacyShip() = delete;
-		~LegacyShip() = default;
-		LegacyShip(const LegacyShip&) = default;
-		LegacyShip(LegacyShip&&) = default;
-		LegacyShip& operator=(const LegacyShip&) = default;
-		LegacyShip& operator=(LegacyShip&&) = default;
+			const std::string& name,
+			const std::string& type,
+			const std::string& equipment,
+			const std::string& owner
+		): Ship(name, type, equipment, owner) {}
 
 		friend std::ostream& operator << (std::ostream& output, const LegacyShip& instance);
 };
@@ -57,4 +28,4 @@ class LegacyShip: public Ship
 
 
 
-#endif // LEGACY_SHIP_H_
+#endif // LEGACY_SHIP_H
