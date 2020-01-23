96c06d57629bfbebe2f0b72349c2338ebced91ea
Idhrendur <idhrendur@hotmail.com>
2020-01-22 23:00:53 -0800

    Clean up parser usage

--- Vic2ToHoI4/Source/HOI4World/Leaders/Advisor.cpp
+++ Vic2ToHoI4/Source/HOI4World/Leaders/Advisor.cpp
@@ -6,15 +6,15 @@
 HoI4::Advisor::Advisor(const std::string& ideology, std::istream& theStream):
 	ideology(ideology)
 {
-	registerKeyword(std::regex("traits"), [this](const std::string& unused, std::istream& theStream){
+	registerKeyword("traits", [this](const std::string& unused, std::istream& theStream){
 		const commonItems::stringList traitString(theStream);
 		traits = traitString.getStrings();
 	});
-	registerKeyword(std::regex("picture"), [this](const std::string& unused, std::istream& theStream){
+	registerKeyword("picture", [this](const std::string& unused, std::istream& theStream){
 		const commonItems::singleString pictureString(theStream);
 		picture = pictureString.getString();
 	});
-	registerKeyword(std::regex("[A-Za-z0-9_]+"), commonItems::ignoreItem);
+	registerRegex("[A-Za-z0-9_]+", commonItems::ignoreItem);
 
 	parseStream(theStream);
 }
--- Vic2ToHoI4/Source/HOI4World/Leaders/IdeologicalAdvisors.cpp
+++ Vic2ToHoI4/Source/HOI4World/Leaders/IdeologicalAdvisors.cpp
@@ -1,14 +1,16 @@
 #include "IdeologicalAdvisors.h"
 #include "Advisor.h"
+#include "ParserHelpers.h"
 
 
 
 HoI4::IdeologicalAdvisors::IdeologicalAdvisors() noexcept
 {
-	registerKeyword(std::regex("[a-z]+"), [this](const std::string& ideology, std::istream& theStream){
+	registerRegex("[a-z]+", [this](const std::string& ideology, std::istream& theStream){
 		Advisor newAdvisor(ideology, theStream);
 		theAdvisors.insert(std::make_pair(ideology, newAdvisor));
 	});
+	registerRegex("[A-Za-z0-9_]+", commonItems::ignoreItem);
 
 	parseFile("ideologicalAdvisors.txt");
 }
