fa410f2325b2aefe919504035ed7284e6b3871b1
Idhrendur <idhrendur@hotmail.com>
2020-02-20 18:31:51 -0800

    Clean up UnitMappingsTests

--- Vic2ToHoI4Tests/HoI4WorldTests/MilitaryMappings/UnitMappingsTests.cpp
+++ Vic2ToHoI4Tests/HoI4WorldTests/MilitaryMappings/UnitMappingsTests.cpp
@@ -1,9 +1,9 @@
-#include "gtest/gtest.h"
 #include "../Vic2ToHoI4/Source/HOI4World/MilitaryMappings/UnitMappings.h"
+#include "gtest/gtest.h"
 
 
 
-TEST(HoI4World_MilitaryMappings_unitMappingsTests, NonExistingMappingNotAdded)
+TEST(HoI4World_MilitaryMappings_unitMappingsTests, NonExistingMappingHasNoMatchingType)
 {
 	std::stringstream input;
 	input << "= {\n";
@@ -13,38 +13,46 @@ TEST(HoI4World_MilitaryMappings_unitMappingsTests, NonExistingMappingNotAdded)
 }
 
 
-TEST(HoI4World_MilitaryMappings_unitMappingsTests, Vic2UnitAddedToUnitMapping)
+TEST(HoI4World_MilitaryMappings_unitMappingsTests, MappingCanBeAdded)
 {
 	std::stringstream input(
-		"= {\n"\
-		"\t\tlink = {\n"\
-		"\t\t\tvic = irregular\n"\
-		"\t\t\thoi = { type = foo }\n"\
-		"\t\t}\n"\
-		"\t}"
-	);
+		 "= {\n"
+		 "\t\tlink = {\n"
+		 "\t\t\tvic = irregular\n"
+		 "\t\t\thoi = { type = foo }\n"
+		 "\t\t}\n"
+		 "\t}");
 	const HoI4::UnitMappings theMappings(input);
 	ASSERT_TRUE(theMappings.hasMatchingType("irregular"));
 }
 
 
-TEST(HoI4World_MilitaryMappings_unitMappingsTests, UnitMappingHandlesFilledHoI4UnitTypeCorrectly)
+TEST(HoI4World_MilitaryMappings_mtgUnitMappingsTests, UnmatchedMappingReturnsNullopt)
+{
+	std::stringstream input;
+	input << "= {\n";
+	input << "}";
+	const HoI4::UnitMappings theMappings(input);
+	ASSERT_FALSE(theMappings.getMatchingUnitInfo("infantry"));
+}
+
+
+TEST(HoI4World_MilitaryMappings_unitMappingsTests, MatchedMappingReturnsType)
 {
 	std::stringstream input(
-		"= {\n"\
-		"\tmap = {\n"\
-		"\t\tlink = {\n"\
-		"\t\t\tvic = infantry\n"\
-		"\t\t\thoi = {\n"\
-		"\t\t\t\ttype = land\n"\
-		"\t\t\t}\n"\
-		"\t\t}\n"\
-		"\t}"\
-		"}"
-	);
+		 "= {\n"
+		 "\tmap = {\n"
+		 "\t\tlink = {\n"
+		 "\t\t\tvic = infantry\n"
+		 "\t\t\thoi = {\n"
+		 "\t\t\t\ttype = land\n"
+		 "\t\t\t}\n"
+		 "\t\t}\n"
+		 "\t}"
+		 "}");
 	const HoI4::UnitMappings theMappings(input);
 
 	auto mapping = theMappings.getMatchingUnitInfo("infantry");
 	ASSERT_TRUE(mapping);
-	ASSERT_EQ(std::string("land"), mapping->getType());
+	ASSERT_EQ("land", mapping->getType());
 }
