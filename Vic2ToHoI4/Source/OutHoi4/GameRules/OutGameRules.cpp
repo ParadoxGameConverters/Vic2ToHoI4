369005788040e288680edbcd0828b85a07e20996
Idhrendur <idhrendur@hotmail.com>
2020-03-31 21:28:00 -0700

    Create game rules directory

--- Vic2ToHoI4/Source/OutHoi4/GameRules/OutGameRules.cpp
+++ Vic2ToHoI4/Source/OutHoi4/GameRules/OutGameRules.cpp
@@ -1,11 +1,20 @@
 #include "OutGameRules.h"
+#include "OSCompatibilityLayer.h"
 #include <fstream>
 
 
 
 void HoI4::outputGameRules(const GameRules& rules, const std::string& outputName)
 {
+	if(!Utils::TryCreateFolder("output/" + outputName + "/common/game_rules"))
+	{
+		throw std::runtime_error{ "Could not create output/" + outputName + "/common/game_rules"};
+	}
 	std::ofstream rulesFile("output/" + outputName + "/common/game_rules/00_game_rules.txt");
+	if (!rulesFile.is_open())
+	{
+		throw std::runtime_error{ "Could not open output/" + outputName + "/common/game_rules/00_game_rules.txt" };
+	}
 
 	for (const auto& rule: rules.getGameRules())
 	{
