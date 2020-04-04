d8728bf6b429084dd57c6ce1f7d3f427f5a25085
Idhrendur <idhrendur@hotmail.com>
2020-04-04 10:44:29 -0700

    Undo pragma optimize

--- Vic2ToHoI4/Source/V2World/World.cpp
+++ Vic2ToHoI4/Source/V2World/World.cpp
@@ -243,7 +243,7 @@ void Vic2::World::determineEmployedWorkers()
 	}
 }
 
-#pragma optimize("",off)
+
 void Vic2::World::removeEmptyNations()
 {
 	std::map<std::string, Country*> newCountries;
@@ -263,7 +263,7 @@ void Vic2::World::removeEmptyNations()
 
 	countries.swap(newCountries);
 }
-#pragma optimize("",on)
+
 
 void Vic2::World::determinePartialStates()
 {
@@ -385,7 +385,7 @@ std::string Vic2::World::extractCountryFileName(const std::string& countryFileLi
 	return countryFileName;
 }
 
-#pragma optimize("",off)
+
 void Vic2::World::overallMergeNations()
 {
 	LOG(LogLevel::Info) << "Merging nations";
@@ -413,7 +413,7 @@ void Vic2::World::mergeNations(const std::string& masterTag, const std::vector<s
 		}
 	}
 }
-#pragma optimize("",on)
+
 
 std::optional<Vic2::Country*> Vic2::World::getCountry(const std::string& tag) const
 {
