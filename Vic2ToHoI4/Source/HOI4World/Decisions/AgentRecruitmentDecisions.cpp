6e4badb9c7baa30c0d21d90e139a3f1542a3bd7c
Idhrendur <idhrendur@hotmail.com>
2020-04-09 10:08:03 -0700

    Import and re-export agent recruitment decisions

---
+++ Vic2ToHoI4/Source/HOI4World/Decisions/AgentRecruitmentDecisions.cpp
@@ -0,0 +1,7 @@
+#include "AgentRecruitmentDecisions.h"
+
+
+
+void HoI4::AgentRecruitmentDecisions::updateDecisions()
+{
+}
--- Vic2ToHoI4/Source/HOI4World/Decisions/Decisions.h
+++ Vic2ToHoI4/Source/HOI4World/Decisions/Decisions.h
@@ -3,7 +3,9 @@
 
 
 
-#include "newParser.h"
+#include "../../Configuration.h"
+#include "../States/DefaultState.h"
+#include "AgentRecruitmentDecisions.h"
 #include "DecisionsCategory.h"
 #include "ExiledGovernmentsDecisions.h"
 #include "ForeignInfluenceDecisions.h"
@@ -12,8 +14,7 @@
 #include "PoliticalDecisions.h"
 #include "ResourceProspectingDecisions.h"
 #include "StabilityWarSupportDecisions.h"
-#include "../States/DefaultState.h"
-#include "../../Configuration.h"
+#include "newParser.h"
 #include <map>
 #include <set>
 #include <string>
@@ -29,57 +30,59 @@ class Events;
 
 class decisions: commonItems::parser
 {
-	public:
-		explicit decisions(const Configuration& theConfiguration);
+  public:
+	explicit decisions(const Configuration& theConfiguration);
 
-		void updateDecisions(
-			const std::set<std::string>& majorIdeologies,
-			const std::map<int, int>& provinceToStateIdMap,
-			const std::map<int, DefaultState>& defaultStates,
-			const Events& theEvents
-		);
+	void updateDecisions(const std::set<std::string>& majorIdeologies,
+		 const std::map<int, int>& provinceToStateIdMap,
+		 const std::map<int, DefaultState>& defaultStates,
+		 const Events& theEvents);
 
-		[[nodiscard]] const std::vector<decisionsCategory>& getStabilityDecisions() const
-		{
-			return stabilityDecisions.getDecisions();
-		}
-		[[nodiscard]] const std::vector<decisionsCategory>& getPoliticalDecisions() const
-		{
-			return politicalDecisions.getDecisions();
-		}
-		[[nodiscard]] const std::vector<decisionsCategory>& getExiledGovernmentsDecisions() const
-		{
-			return exiledGovernmentsDecisions.getDecisions();
-		}
-		[[nodiscard]] const std::vector<decisionsCategory>& getForeignInfluenceDecisions() const
-		{
-			return foreignInfluenceDecisions.getDecisions();
-		}
-		[[nodiscard]] const std::vector<decisionsCategory>& getNavalTreatyDecisions() const
-		{
-			return navalTreatyDecisions.getDecisions();
-		}
-		[[nodiscard]] const std::vector<decisionsCategory>& getResourceProspectingDecisions() const
-		{
-			return resourceProspectingDecisions.getDecisions();
-		}
-		[[nodiscard]] const std::vector<decisionsCategory>& getGenericDecisions() const
-		{
-			return genericDecisions.getDecisions();
-		}
+	[[nodiscard]] const std::vector<decisionsCategory>& getAgentRecruitmentDecisions() const
+	{
+		return agentRecruitmentDecisions.getDecisions();
+	}
+	[[nodiscard]] const std::vector<decisionsCategory>& getStabilityDecisions() const
+	{
+		return stabilityDecisions.getDecisions();
+	}
+	[[nodiscard]] const std::vector<decisionsCategory>& getPoliticalDecisions() const
+	{
+		return politicalDecisions.getDecisions();
+	}
+	[[nodiscard]] const std::vector<decisionsCategory>& getExiledGovernmentsDecisions() const
+	{
+		return exiledGovernmentsDecisions.getDecisions();
+	}
+	[[nodiscard]] const std::vector<decisionsCategory>& getForeignInfluenceDecisions() const
+	{
+		return foreignInfluenceDecisions.getDecisions();
+	}
+	[[nodiscard]] const std::vector<decisionsCategory>& getNavalTreatyDecisions() const
+	{
+		return navalTreatyDecisions.getDecisions();
+	}
+	[[nodiscard]] const std::vector<decisionsCategory>& getResourceProspectingDecisions() const
+	{
+		return resourceProspectingDecisions.getDecisions();
+	}
+	[[nodiscard]] const std::vector<decisionsCategory>& getGenericDecisions() const
+	{
+		return genericDecisions.getDecisions();
+	}
 
-	private:
-		StabilityWarSupportDecisions stabilityDecisions;
-		PoliticalDecisions politicalDecisions;
-		ExiledGovernmentsDecisions exiledGovernmentsDecisions;
-		ForeignInfluenceDecisions foreignInfluenceDecisions;
-		NavalTreatyDecisions navalTreatyDecisions;
-		ResourceProspectingDecisions resourceProspectingDecisions;
-		GenericDecisions genericDecisions;
+  private:
+	AgentRecruitmentDecisions agentRecruitmentDecisions;
+	StabilityWarSupportDecisions stabilityDecisions;
+	PoliticalDecisions politicalDecisions;
+	ExiledGovernmentsDecisions exiledGovernmentsDecisions;
+	ForeignInfluenceDecisions foreignInfluenceDecisions;
+	NavalTreatyDecisions navalTreatyDecisions;
+	ResourceProspectingDecisions resourceProspectingDecisions;
+	GenericDecisions genericDecisions;
 };
 
-}
-
+} // namespace HoI4
 
 
 
