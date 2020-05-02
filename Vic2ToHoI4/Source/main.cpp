261d898d173d77a814293039dd877fe6e7012cb7
Idhrendur <idhrendur@hotmail.com>
2020-05-02 11:08:41 -0700

    Style stuff on main

--- Vic2ToHoI4/Source/main.cpp
+++ Vic2ToHoI4/Source/main.cpp
@@ -1,30 +1,7 @@
-/*Copyright (c) 2018 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
 #include "Log.h"
-#include "Vic2ToHoI4Converter.h"
-#include "OSCompatibilityLayer.h"
 #include "Mappers/VersionParser/VersionParser.h"
+#include "OSCompatibilityLayer.h"
+#include "Vic2ToHoI4Converter.h"
 
 
 
@@ -33,21 +10,21 @@ int main(const int argc, const char* argv[])
 	try
 	{
 		const mappers::VersionParser versionParser;
-		LOG(LogLevel::Info) << versionParser;
-		LOG(LogLevel::Info) << "Built on " << __TIMESTAMP__;
-		LOG(LogLevel::Info) << "Current directory is " << Utils::getCurrentDirectory();
+		Log(LogLevel::Info) << versionParser;
+		Log(LogLevel::Info) << "Built on " << __TIMESTAMP__;
+		Log(LogLevel::Info) << "Current directory is " << Utils::getCurrentDirectory();
 
 		const char* const defaultV2SaveFileName = "input.v2";
 		std::string V2SaveFileName;
 		if (argc >= 2)
 		{
 			V2SaveFileName = argv[1];
-			LOG(LogLevel::Info) << "Using input file " << V2SaveFileName;
+			Log(LogLevel::Info) << "Using input file " << V2SaveFileName;
 		}
 		else
 		{
 			V2SaveFileName = defaultV2SaveFileName;
-			LOG(LogLevel::Info) << "No input file given, defaulting to " << defaultV2SaveFileName;
+			Log(LogLevel::Info) << "No input file given, defaulting to " << defaultV2SaveFileName;
 		}
 
 		ConvertV2ToHoI4(V2SaveFileName);
@@ -57,7 +34,7 @@ int main(const int argc, const char* argv[])
 
 	catch (const std::exception& e)
 	{
-		LOG(LogLevel::Error) << e.what();
+		Log(LogLevel::Error) << e.what();
 		return -1;
 	}
 }
