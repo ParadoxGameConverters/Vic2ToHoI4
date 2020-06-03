e6c1fdba01dd8df4b1e1ddba3edd68a8d532d471
Idhrendur <idhrendur@hotmail.com>
2020-06-02 23:07:49 -0700

    If no democracy, strip the modifier from no_garrison's ai_will_do

--- Vic2ToHoI4Tests/HoI4WorldTests/OccupationLaws/OccupationLawsTests.cpp
+++ Vic2ToHoI4Tests/HoI4WorldTests/OccupationLaws/OccupationLawsTests.cpp
@@ -3,6 +3,53 @@
 
 
 
+TEST(HoI4World_OccupationLaws_OccupationLaws, NoGarrisonSimplifiesAiWillDoIfNoDemocratic)
+{
+	HoI4::OccupationLaw noGarrison;
+	noGarrison.setName("no_garrison");
+
+	HoI4::OccupationLaws laws;
+	laws.giveOccupationLaw(std::move(noGarrison));
+
+	laws.updateLaws({});
+	ASSERT_FALSE(laws.getOccupationLaws().empty());
+	ASSERT_EQ(
+		 "= {\n"
+		 "\t\tbase = 0\n"
+		 "\n"
+		 "\t\t# if losing, get manpower & equipment back\n"
+		 "\t\tmodifier = {\n"
+		 "\t\t\tFROM = {\n"
+		 "\t\t\t\tsurrender_progress > 0.15\n"
+		 "\t\t\t}\n"
+		 "\t\t\tadd = 100\n"
+		 "\t\t}\n"
+		 "\t\tmodifier = {\n"
+		 "\t\t\tFROM = {\n"
+		 "\t\t\t\tsurrender_progress > 0.4\n"
+		 "\t\t\t}\n"
+		 "\t\t\tadd = 500\n"
+		 "\t\t}\n"
+		 "\t}",
+		 laws.getOccupationLaws()[0].getAiWillDo());
+}
+
+
+TEST(HoI4World_OccupationLaws_OccupationLaws, NoGarrisonLeavesAiWillDoIfDemocratic)
+{
+	HoI4::OccupationLaw noGarrison;
+	noGarrison.setName("no_garrison");
+	noGarrison.setAiWillDo(" = { test }");
+
+	HoI4::OccupationLaws laws;
+	laws.giveOccupationLaw(std::move(noGarrison));
+
+	laws.updateLaws({"democratic"});
+	ASSERT_FALSE(laws.getOccupationLaws().empty());
+	ASSERT_EQ(" = { test }", laws.getOccupationLaws()[0].getAiWillDo());
+}
+
+
 TEST(HoI4World_OccupationLaws_OccupationLaws, AutonomousOccupationRemovedIfNoDemocractic)
 {
 	HoI4::OccupationLaw autonomousOccupation;
