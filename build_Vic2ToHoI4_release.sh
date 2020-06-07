fd2f64572816abdb80ccc68f70b000bcc2e902e8
Idhrendur <idhrendur@hotmail.com>
2020-06-06 22:07:28 -0700

    Linux build improvements (#663)
    * Minor tweaks to CMakeLists.txt
    * Add CMakeLists.txt for tests
    * Make gitignore more robust
    * Consolidate CMakeLists
    * Add remaining tests
    * Clean up CMakeLists
    * Fix copying files
    * A fix for travis building

--- build_Vic2ToHoI4_release.sh
+++ build_Vic2ToHoI4_release.sh
@@ -4,12 +4,11 @@ read version
 rm -rf "Vic2ToHoI4-$version"
 rm -rf "Vic2ToHoI4-$version.tar.bz2"
 
-cd Vic2ToHoI4
 rm -rf build
 rm -rf Release-Linux
+rm -rf Release-Tests
 cmake -H. -Bbuild
 cmake --build ./build -- -j3
-cp -rf "Release-Linux" "../Vic2ToHoI4-$version"
-cd ..
+cp -rf "Release-Linux" "Vic2ToHoI4-$version"
 
 tar -cjf "Vic2ToHoI4-$version.tar.bz2" "Vic2ToHoI4-$version"
