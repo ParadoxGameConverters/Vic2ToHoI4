73963ec63ab9119379bac8488136e9fc8b230283
Idhrendur <idhrendur@hotmail.com>
2020-08-12 18:22:30 -0700

    Clean up the ai_will_do for the instantiate_collaboration decision. (#683)

--- Vic2ToHoI4/Source/HOI4World/Decisions/ForeignInfluenceDecisions.cpp
+++ Vic2ToHoI4/Source/HOI4World/Decisions/ForeignInfluenceDecisions.cpp
@@ -29,6 +29,63 @@ void HoI4::ForeignInfluenceDecisions::updateDecisions(const std::set<std::string
 		{
 			if (decision.getName() == "instantiate_collaboration")
 			{
+				std::string aiWillDo;
+				aiWillDo += "= {\n";
+				aiWillDo += "\t\t\tbase = 10\n";
+				aiWillDo += "\t\t\t# priotize larger countries\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\tmodifier = {\n";
+				aiWillDo += "\t\t\t\t# neutrals won't do collaboration until 1944 and they are at peace\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t\thas_government = neutrality\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t\tOR = {\n";
+				aiWillDo += "\t\t\t\t\thas_war = yes\n";
+				aiWillDo += "\t\t\t\t\tdate < 1944.01.01\n";
+				aiWillDo += "\t\t\t\t}\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t\tfactor = 0\n";
+				aiWillDo += "\t\t\t}\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\tmodifier = {\n";
+				aiWillDo += "\t\t\t\t# calculating cores we control\n";
+				aiWillDo += "\t\t\t\tset_temp_variable = { num_cores_we_control = 0 }\n";
+				aiWillDo += "\t\t\t\tall_of_scopes = {\n";
+				aiWillDo += "\t\t\t\t\tarray = FROM.core_states\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t\t\tif = {\n";
+				aiWillDo += "\t\t\t\t\t\tlimit = { is_controlled_by = PREV }\n";
+				aiWillDo += "\t\t\t\t\t\tadd_to_temp_variable = { num_cores_we_control = 1 }\n";
+				aiWillDo += "\t\t\t\t\t}\n";
+				aiWillDo += "\t\t\t\t}\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t\t# more score if more cores are controlled by us\n";
+				aiWillDo += "\t\t\t\tadd = num_cores_we_control\n";
+				aiWillDo += "\t\t\t}\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t# dont make too many collaboratios\n";
+				aiWillDo += "\t\t\tmodifier = {\n";
+				aiWillDo += "\t\t\t\t# as number of collaboration subjects increases\n";
+				aiWillDo += "\t\t\t\t# only accept subjects with more cores\n";
+				aiWillDo += "\t\t\t\tset_temp_variable = { num_min_cores = 1.5 }\n";
+				aiWillDo += "\t\t\t\tall_of_scopes = {\n";
+				aiWillDo += "\t\t\t\t\tarray = subjects\n";
+				aiWillDo += "\t\t\t\t\tif = {\n";
+				aiWillDo += "\t\t\t\t\t\tlimit = {\n";
+				aiWillDo += "\t\t\t\t\t\t\thas_autonomy_state = autonomy_collaboration_government\n";
+				aiWillDo += "\t\t\t\t\t\t}\n";
+				aiWillDo += "\t\t\t\t\t\tadd_to_temp_variable = { num_min_cores = 0.7 }\n";
+				aiWillDo += "\t\t\t\t\t}\n";
+				aiWillDo += "\t\t\t\t}\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t\tcheck_variable = { num_cores_we_control < num_min_cores }\n";
+				aiWillDo += "\n";
+				aiWillDo += "\t\t\t\tfactor = 0\n";
+				aiWillDo += "\t\t\t}\n";
+				aiWillDo += "\t\t}";
+				decision.setAiWillDo(aiWillDo);
+				category.replaceDecision(decision);
+
 				continue;
 			}
 
@@ -57,8 +114,8 @@ void HoI4::ForeignInfluenceDecisions::updateDecisions(const std::set<std::string
 				newVisible += "\t\t\t}\n";
 				newVisible += "\t\t}";
 			}
-
 			decision.setVisible(newVisible);
+
 			category.replaceDecision(decision);
 		}
 	}
