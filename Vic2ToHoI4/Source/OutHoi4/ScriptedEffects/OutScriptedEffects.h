c68dc38dddf7884d62207237416d124eec75f58c
Idhrendur <idhrendur@hotmail.com>
2020-08-18 20:13:26 -0700

    Scripted effects (#690)
    * Basic setup to update 00_scripted_effects.txt
    * Add get_best_alliance_match_<ideology>_effect
    * Add remove_from_allowed_party
    * ReSharper fix
    * Build on Linux

---
+++ Vic2ToHoI4/Source/OutHoi4/ScriptedEffects/OutScriptedEffects.h
@@ -0,0 +1,20 @@
+#ifndef OUT_SCRIPTED_EFFECTS
+#define OUT_SCRIPTED_EFFECTS
+
+
+
+#include <set>
+#include <string>
+
+
+
+namespace HoI4
+{
+
+void outputScriptedEffects(const std::set<std::string>& majorIdeologies, const std::string& outputName);
+
+}
+
+
+
+#endif // OUT_SCRIPTED_EFFECTS
