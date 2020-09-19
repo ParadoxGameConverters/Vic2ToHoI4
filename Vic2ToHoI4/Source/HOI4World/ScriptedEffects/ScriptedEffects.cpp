f7b96f03ade401bfdceabc24b4ff98e4cfbdff34
Idhrendur <idhrendur@hotmail.com>
2020-09-19 15:23:22 -0700

    Codacy

--- Vic2ToHoI4/Source/HOI4World/ScriptedEffects/ScriptedEffects.cpp
+++ Vic2ToHoI4/Source/HOI4World/ScriptedEffects/ScriptedEffects.cpp
@@ -4,10 +4,10 @@
 
 
 
-HoI4::ScriptedEffects::ScriptedEffects(const std::string& HoI4Location)
+HoI4::ScriptedEffects::ScriptedEffects(const std::string& HoI4Location):
+	 operationStratEffects(
+		  ScriptedEffectFile{}.getScriptedEffects(HoI4Location + "/common/scripted_effects/operation_strat_effects.txt"))
 {
-	operationStratEffects =
-		 ScriptedEffectFile{}.getScriptedEffects(HoI4Location + "/common/scripted_effects/operation_strat_effects.txt");
 }
 
 
