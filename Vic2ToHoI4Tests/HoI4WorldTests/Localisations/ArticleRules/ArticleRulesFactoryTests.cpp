a46073c81f7fc0f4d344005977be53dbfb674604
Idhrendur <idhrendur@hotmail.com>
2020-09-26 00:15:55 -0700

    Remove some debugging

--- Vic2ToHoI4Tests/HoI4WorldTests/Localisations/ArticleRules/ArticleRulesFactoryTests.cpp
+++ Vic2ToHoI4Tests/HoI4WorldTests/Localisations/ArticleRules/ArticleRulesFactoryTests.cpp
@@ -3,11 +3,10 @@
 #include <sstream>
 
 
-#pragma optimize("",off)
+
 TEST(HoI4World_Localisations_ArticleRules_ArticleRulesFactory, LanguageDefaultsToEmpty)
 {
 	const auto rules = HoI4::ArticleRules::Factory{}.getRules("ArticleRules.txt");
 
 	ASSERT_EQ("Matching String", rules->updateArticles("english", "The Matching String"));
 }
-#pragma optimize("", on)
