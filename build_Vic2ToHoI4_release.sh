9d2be6406d1e6683149eac798ecde4892ee93def
Idhrendur <idhrendur@hotmail.com>
2020-10-04 16:44:07 -0700

    Turn on github's security scanner (#750)
    * Turn on github's security scanner
    * Update codeql-analysis.yml
    * Update build_Vic2ToHoI4_release.sh
    * Update codeql-analysis.yml

--- build_Vic2ToHoI4_release.sh
+++ build_Vic2ToHoI4_release.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-echo "Version Number?"
-read version
-rm -rf "Vic2ToHoI4-$version"
-rm -rf "Vic2ToHoI4-$version.tar.bz2"
-
-rm -rf build
-rm -rf Release-Linux
-rm -rf Release-Tests
 cmake -H. -Bbuild
 cmake --build ./build -- -j3
-cp -rf "Release-Linux" "Vic2ToHoI4-$version"
-
-tar -cjf "Vic2ToHoI4-$version.tar.bz2" "Vic2ToHoI4-$version"
