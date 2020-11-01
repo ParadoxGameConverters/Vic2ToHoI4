2f825104cce6b9f67887689d79a47f5878ba0acc
Idhrendur <idhrendur@hotmail.com>
2020-11-01 15:33:40 -0800

    Logistics skill from Vic2 organisation

--- Vic2ToHoI4/Source/HOI4World/Leaders/General.cpp
+++ Vic2ToHoI4/Source/HOI4World/Leaders/General.cpp
@@ -1,10 +1,10 @@
 #include "General.h"
-
+#include <algorithm>
 
 
 HoI4::General::General(const Vic2::Leader& srcLeader, const std::string& portrait):
 	Commander(srcLeader, portrait)
 {
 	planningSkill = varySkill(skill);
-	logisticsSkill = varySkill(skill);
+	logisticsSkill = std::clamp(static_cast<int>(std::round(srcLeader.getTraitEffectValue("organisation") * 25.0F)) + 1, 1, 5);
 }
