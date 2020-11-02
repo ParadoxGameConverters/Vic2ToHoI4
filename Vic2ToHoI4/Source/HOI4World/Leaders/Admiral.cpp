c27549f3bc9adeb787e6b1c31ee73a3a851db084
Idhrendur <idhrendur@hotmail.com>
2020-11-01 17:25:32 -0800

    Coordination skill comes from Vic2 organisation

--- Vic2ToHoI4/Source/HOI4World/Leaders/Admiral.cpp
+++ Vic2ToHoI4/Source/HOI4World/Leaders/Admiral.cpp
@@ -1,10 +1,10 @@
 #include "Admiral.h"
+#include <algorithm>
 
 
 
-HoI4::Admiral::Admiral(const Vic2::Leader& srcLeader, const std::string& portrait):
-	Commander(srcLeader, portrait)
+HoI4::Admiral::Admiral(const Vic2::Leader& srcLeader, const std::string& portrait): Commander(srcLeader, portrait)
 {
 	maneuveringSkill = varySkill(skill);
-	coordinationSkill = varySkill(skill);
+	coordinationSkill = std::clamp(static_cast<int>(std::round(srcLeader.getTraitEffectValue("organisation") * 25.0F)) + 1, 1, 7);
 }
