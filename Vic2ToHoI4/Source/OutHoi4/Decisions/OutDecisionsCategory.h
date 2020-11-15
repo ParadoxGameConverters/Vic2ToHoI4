c2fed3b66f4f0edd82809ed2bbd074f2b705f308
IlikeTrains <sewerynpresnal@gmail.com>
2020-11-15 01:16:49 +0100

    Error fix

--- Vic2ToHoI4/Source/OutHoi4/Decisions/OutDecisionsCategory.h
+++ Vic2ToHoI4/Source/OutHoi4/Decisions/OutDecisionsCategory.h
@@ -7,8 +7,10 @@
 #include <ostream>
 
 
-
-std::ostream& HoI4::operator<<(std::ostream& out, const DecisionsCategory& decisionsCategory);
+namespace HoI4
+{
+std::ostream& operator<<(std::ostream& out, const DecisionsCategory& decisionsCategory);
+}
 
 
 
