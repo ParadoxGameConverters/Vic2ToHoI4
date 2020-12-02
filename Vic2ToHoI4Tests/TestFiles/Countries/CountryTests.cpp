579d96979de07dc4b2ccc1dd9e139eae0f255961
Idhrendur <idhrendur@hotmail.com>
2020-12-01 17:09:01 -0800

    Get initial country tests functional

--- Vic2ToHoI4Tests/TestFiles/Countries/CountryTests.cpp
+++ Vic2ToHoI4Tests/TestFiles/Countries/CountryTests.cpp
@@ -2,8 +2,8 @@
 #include "V2World/Countries/CommonCountryDataBuilder.h"
 #include "V2World/Countries/Country.h"
 #include "V2World/Countries/CountryFactory.h"
-#include "V2World/States/StateDefinitionsBuilder.h"
 #include "V2World/Politics/PartyBuilder.h"
+#include "V2World/States/StateDefinitionsBuilder.h"
 #include "gtest/gtest.h"
 #include <sstream>
 
@@ -15,13 +15,31 @@ TEST(Vic2World_Countries_CountryTests, tagIsAsSet)
 	theStream << "= {\n";
 	theStream << "\truling_party = 1";
 	theStream << "}";
-	const auto country = Vic2::Country::Factory
-	{*Configuration::Builder{}.setVic2Path("./countries/blank/").build(), *Vic2::StateDefinitions::Builder{}.build()}
-			  .createCountry(
-					"TAG",
-					theStream,
-					*Vic2::CommonCountryData::Builder{}.Build(),
-					std::vector<Vic2::Party> { *Vic2::Party::Builder{}.Build() });
+	const auto country = Vic2::Country::Factory{*Configuration::Builder{}.setVic2Path("./countries/blank/").build(),
+		 *Vic2::StateDefinitions::Builder{}.build()}
+									 .createCountry("TAG",
+										  theStream,
+										  *Vic2::CommonCountryData::Builder{}.Build(),
+										  std::vector<Vic2::Party>{*Vic2::Party::Builder{}.Build()});
 
 	ASSERT_EQ("TAG", country->getTag());
+}
+
+
+TEST(Vic2World_Countries_CountryTests, colorIsFromCommonCountryData)
+{
+	const commonItems::Color testColor{std::array<int,3>{1,2,3}};
+	
+	std::stringstream theStream;
+	theStream << "= {\n";
+	theStream << "\truling_party = 1";
+	theStream << "}";
+	const auto country = Vic2::Country::Factory{*Configuration::Builder{}.setVic2Path("./countries/blank/").build(),
+		 *Vic2::StateDefinitions::Builder{}.build()}
+									 .createCountry("TAG",
+										  theStream,
+										  *Vic2::CommonCountryData::Builder{}.setColor(testColor).Build(),
+										  std::vector<Vic2::Party>{*Vic2::Party::Builder{}.Build()});
+
+	ASSERT_EQ(testColor, country->getColor());
 }
