e8683308a5bdf39997190371114c8db4593b3467
Idhrendur <idhrendur@hotmail.com>
2021-07-10 19:17:00 -0700

    Pheonix scale9094 faction name mapper (#1071)
    * Configurable Faction Naming
    
    * Mapping Fixes
    
    * Various fixes
    
    *Fixed unused return value of remove_if
    *Fixed new files not being included
    
    * Added tests
    
    Fixed Linker
    Added Linux support
    Added Tests
    
    * Update CMakeLists.txt
    
    * Fixes
    
    Added duplicate mapping removal test
    
    * CMake Fix
    
    * Fix windows build
    
    Co-authored-by: PheonixScale9094 <64274130+PheonixScale9094@users.noreply.github.com>

---
+++ Vic2ToHoI4/Source/Mappers/FactionName/FactionNameMappingFactory.h
@@ -0,0 +1,28 @@
+#ifndef FACTION_NAME_MAPPING_FACTORY_H
+#define FACTION_NAME_MAPPING_FACTORY_H
+
+
+
+#include "FactionNameMapping.h"
+#include "Parser.h"
+#include <memory>
+
+
+namespace Mappers
+{
+
+class FactionNameMappingFactory: commonItems::parser
+{
+  public:
+	FactionNameMappingFactory();
+	std::unique_ptr<FactionNameMapping> importMapping(std::istream& theStream);
+
+  private:
+	std::unique_ptr<FactionNameMapping> factionNameMapping;
+};
+
+} // namespace Mappers
+
+
+
+#endif // FACTION_NAME_MAPPING_FACTORY_H
