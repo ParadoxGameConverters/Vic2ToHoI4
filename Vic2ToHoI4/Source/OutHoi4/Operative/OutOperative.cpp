cd5389586585a831c372ee55bef778eb84252a68
CarbonY26 <83087301+CarbonY26@users.noreply.github.com>
2021-07-14 23:53:14 -0500

    Seperating portraits and mapping operators (#1045)
    * The tedious part
    
    * Mappings
    
    * Operators for animal cultures
    
    * Update cultureGroupToGraphics.txt
    
    * Update cultureGroupToGraphics.txt
    
    * Update cultureGroupToGraphics.txt
    
    * Update cultureGroupToGraphics.txt
    
    * Update cultureGroupToGraphics.txt
    
    * Update cultureGroupToGraphics.txt
    
    * Update cultureGroupToGraphics.txt
    
    * Finally done with this part
    
    * Update 00_portraits.txt
    
    * Update 00_portraits.txt
    
    * Time to redo
    
    * Completed 00_portraits.txt
    
    * Update cultureGroupToGraphics.txt
    
    * Support operative portraits with spaces in the path
    
    * Update 00_portraits.txt
    
    Co-authored-by: Idhrendur <idhrendur@hotmail.com>

--- Vic2ToHoI4/Source/OutHoi4/Operative/OutOperative.cpp
+++ Vic2ToHoI4/Source/OutHoi4/Operative/OutOperative.cpp
@@ -7,7 +7,7 @@ std::ostream& HoI4::operator<<(std::ostream& out, const Operative& operative)
 {
 	out << "\tcreate_operative_leader = {\n";
 	out << "\t\tname = \"" << operative.getName() << "\"\n";
-	out << "\t\tGFX = " << operative.getPortrait() << "\n";
+	out << "\t\tGFX = \"" << operative.getPortrait() << "\"\n";
 	out << "\t\ttraits = {}\n";
 	out << "\t\tbypass_recruitment = no\n";
 	out << "\t\tavailable_to_spy_master = yes\n";
