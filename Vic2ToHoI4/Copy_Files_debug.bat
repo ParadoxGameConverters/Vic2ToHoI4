1e0567094efaa12d5b1a7cadc2d23e174af9fe06
Idhrendur <idhrendur@hotmail.com>
2021-08-14 16:42:35 -0700

    VS now handles copying blankMod

--- Vic2ToHoI4/Copy_Files_debug.bat
+++ Vic2ToHoI4/Copy_Files_debug.bat
@@ -2,9 +2,4 @@ copy "Resources\msvcp140_codecvt_ids.dll" "..\Debug\Vic2ToHoI4\"
 copy "Resources\vcruntime140_1.dll" "..\Debug\Vic2ToHoI4\"
 copy "Resources\VC_redist.x64.exe" "..\Debug\Vic2ToHoI4\"
 
-rem **Create blankMod**
-del "..\Debug\Vic2ToHoI4\blankMod" /Q /S /F
-rmdir "..\Debug\Vic2ToHoI4\blankMod" /Q /S
-xcopy "Data_Files\blankMod" "..\Debug\Vic2ToHoI4\blankmod" /Y /E /I
-
 git rev-parse HEAD > ..\Debug\commit_id.txt
