0c395d9ed0fb182271663073b77a9f4a2d349b16
Idhrendur <idhrendur@hotmail.com>
2021-08-14 16:51:31 -0700

    VS handles resources

--- Vic2ToHoI4/Copy_Files.bat
+++ Vic2ToHoI4/Copy_Files.bat
@@ -1,5 +1,1 @@
-copy "Resources\msvcp140_codecvt_ids.dll" "..\Release\Vic2ToHoI4\"
-copy "Resources\vcruntime140_1.dll" "..\Release\Vic2ToHoI4\"
-copy "Resources\VC_redist.x64.exe" "..\Release\Vic2ToHoI4\"
-
 git rev-parse HEAD > ..\Release\commit_id.txt
--- Vic2ToHoI4/Copy_Files_debug.bat
+++ Vic2ToHoI4/Copy_Files_debug.bat
@@ -1,5 +1,1 @@
-copy "Resources\msvcp140_codecvt_ids.dll" "..\Debug\Vic2ToHoI4\"
-copy "Resources\vcruntime140_1.dll" "..\Debug\Vic2ToHoI4\"
-copy "Resources\VC_redist.x64.exe" "..\Debug\Vic2ToHoI4\"
-
 git rev-parse HEAD > ..\Debug\commit_id.txt
