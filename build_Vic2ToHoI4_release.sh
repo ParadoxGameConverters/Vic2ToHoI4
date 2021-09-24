4068542cc2f72ae963ed551481fb42e2d3397ff1
Ivo Elezović <ivo@elezovic.su>
2021-09-24 03:58:10 +0200

    Update workflow (#1189)

--- build_Vic2ToHoI4_release.sh
+++ build_Vic2ToHoI4_release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake -H. -Bbuild && cmake --build ./build -- -j9 && cd Release-Tests && ./Vic2ToHoi4ConverterTests && cd ..
+cmake -H. -Bbuild && cmake --build ./build -- -j40 && cd Release-Tests && ./Vic2ToHoi4ConverterTests && cd ..
