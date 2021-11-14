93bb547cfb74092523089d72c35aba90b6139746
Idhrendur <idhrendur@hotmail.com>
2021-11-14 12:00:35 -0800

    Update exiled governments decisions

--- Vic2ToHoI4/Source/HOI4World/Decisions/ExiledGovernmentsDecisions.cpp
+++ Vic2ToHoI4/Source/HOI4World/Decisions/ExiledGovernmentsDecisions.cpp
@@ -3,85 +3,109 @@
 
 
 
-bool exiledGovernmentDecisionToUpdate(const std::string& decisionName);
+HoI4::decision&& updatePurgeInfiltrators(HoI4::decision&& theDecision, const std::set<std::string>& majorIdeologies)
+{
+	std::stringstream available;
+	available << "= {\n";
+	available << "\t\t\tFROM = {\n";
+	available << "\t\t\t\thas_legitimacy > 50\n";
+	available << "\t\t\t\tOR = {\n";
+	for (const auto& ideology: majorIdeologies)
+	{
+		if ((ideology == "democratic") || (ideology == "neutrality"))
+		{
+			continue;
+		}
+		available << "\t\t\t\t\t" << ideology << " > 0.1\n";
+	}
+	available << "\t\t\t\t}\n";
+	available << "\t\t\t}\n";
+	available << "\t\t}";
+	theDecision.setAvailable(available.str());
+
+	std::stringstream visibleAndTT;
+	visibleAndTT << "= {\n";
+	visibleAndTT << "\t\t\tFROM = {\n";
+	visibleAndTT << "\t\t\t\tis_exiled_in = ROOT\n";
+	visibleAndTT << "\t\t\t\thas_legitimacy > 40\n";
+	visibleAndTT << "\t\t\t\tOR = {\n";
+	for (const auto& ideology: majorIdeologies)
+	{
+		if ((ideology == "democratic") || (ideology == "neutrality"))
+		{
+			continue;
+		}
+		visibleAndTT << "\t\t\t\t\t" << ideology << " > 0.05\n";
+	}
+	visibleAndTT << "\t\t\t\t}\n";
+	visibleAndTT << "\t\t\t}\n";
+	visibleAndTT << "\t\t}";
+	theDecision.setVisible(visibleAndTT.str());
+	theDecision.setTargetTrigger(visibleAndTT.str());
+
+	std::stringstream targetedModifier;
+	targetedModifier << "= {\n";
+	targetedModifier << "\t\t\ttag = FROM\n";
+	targetedModifier << "\t\t\ttargeted_legitimacy_daily = -1\n";
+	if (majorIdeologies.contains("democratic"))
+	{
+		targetedModifier << "\t\t\tdemocratic_drift = 0.05\n";
+	}
+	else
+	{
+		targetedModifier << "\t\t\tneutrality_drift = 0.05\n";
+	}
+	targetedModifier << "\t\t}";
+	theDecision.setTargetedModifier(targetedModifier.str());
+
+	return std::move(theDecision);
+}
+
+
+HoI4::decision&& updateExileRecruitmentCampaign(HoI4::decision&& theDecision)
+{
+	std::stringstream targetTrigger;
+	targetTrigger << "= {\n";
+	targetTrigger << "\t\t\tFROM = { is_hosting_exile = ROOT }\n";
+	targetTrigger << "\t\t}";
+	theDecision.setTargetTrigger(targetTrigger.str());
+
+	return std::move(theDecision);
+}
+
+
+HoI4::decision&& updateExileExtractionCampaigns(HoI4::decision&& theDecision)
+{
+	std::stringstream targetTrigger;
+	targetTrigger << "= {\n";
+	targetTrigger << "\t\t\tFROM = { is_exiled_in = ROOT }\n";
+	targetTrigger << "\t\t}";
+	theDecision.setTargetTrigger(targetTrigger.str());
+
+	return std::move(theDecision);
+}
 
 
 void HoI4::ExiledGovernmentsDecisions::updateDecisions(const std::set<std::string>& majorIdeologies)
 {
 	for (auto category: decisions)
 	{
-		auto updated = false;
 		for (auto decision: category.getDecisions())
 		{
-			if (exiledGovernmentDecisionToUpdate(decision.getName()))
+			if (decision.getName() == "purge_infiltrators")
 			{
-				std::stringstream available;
-				available << "= {\n";
-				available << "\t\t\tFROM = {\n";
-				available << "\t\t\t\thas_legitimacy > 50\n";
-				available << "\t\t\t\tOR = {\n";
-				for (const auto& ideology: majorIdeologies)
-				{
-					if ((ideology == "democratic") || (ideology == "neutrality"))
-					{
-						continue;
-					}
-					available << "\t\t\t\t\t" << ideology << " > 0.1\n";
-				}
-				available << "\t\t\t\t}\n";
-				available << "\t\t\t}\n";
-				available << "\t\t}";
-				decision.setAvailable(available.str());
-
-				std::stringstream visibleAndTT;
-				visibleAndTT << "= {\n";
-				visibleAndTT << "\t\t\tFROM = {\n";
-				visibleAndTT << "\t\t\t\tis_exiled_in = ROOT\n";
-				visibleAndTT << "\t\t\t\thas_legitimacy > 40\n";
-				visibleAndTT << "\t\t\t\tOR = {\n";
-				for (const auto& ideology: majorIdeologies)
-				{
-					if ((ideology == "democratic") || (ideology == "neutrality"))
-					{
-						continue;
-					}
-					visibleAndTT << "\t\t\t\t\t" << ideology << " > 0.05\n";
-				}
-				visibleAndTT << "\t\t\t\t}\n";
-				visibleAndTT << "\t\t\t}\n";
-				visibleAndTT << "\t\t}";
-				decision.setVisible(visibleAndTT.str());
-				decision.setTargetTrigger(visibleAndTT.str());
-
-				std::stringstream targetedModifier;
-				targetedModifier << "= {\n";
-				targetedModifier << "\t\t\ttag = FROM\n";
-				targetedModifier << "\t\t\ttargeted_legitimacy_daily = -1\n";
-				if (majorIdeologies.contains("democratic"))
-				{
-					targetedModifier << "\t\t\tdemocratic_drift = 0.05\n";
-				}
-				else
-				{
-					targetedModifier << "\t\t\tneutrality_drift = 0.05\n";
-				}
-				targetedModifier << "\t\t}";
-				decision.setTargetedModifier(targetedModifier.str());
-
-				category.replaceDecision(decision);
-				updated = true;
-				break;
+				category.replaceDecision(updatePurgeInfiltrators(std::move(decision), majorIdeologies));
+			}
+			else if (decision.getName() == "exile_recruitment_campaign")
+			{
+				category.replaceDecision(updateExileRecruitmentCampaign(std::move(decision)));
+			}
+			else if (decision.getName() == "exile_extraction_campaign")
+			{
+				category.replaceDecision(updateExileExtractionCampaigns(std::move(decision)));
 			}
 		}
-		if (updated)
-		{
-			std::replace(decisions.begin(), decisions.end(), category, category);
-		}
+
+		std::ranges::replace(decisions, category, category);
 	}
-}
-
-
-bool exiledGovernmentDecisionToUpdate(const std::string& decisionName)
-{
-	return (decisionName == "purge_infiltrators");
 }
