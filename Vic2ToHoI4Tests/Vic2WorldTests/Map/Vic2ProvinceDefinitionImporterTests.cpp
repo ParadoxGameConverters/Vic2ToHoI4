2b88d9bb9220aec9ff6346eb67e2e4da1d0eb80c
Idhrendur <idhrendur@hotmail.com>
2021-12-08 20:32:23 -0800

    Fix broken tests

--- Vic2ToHoI4Tests/Vic2WorldTests/Map/Vic2ProvinceDefinitionImporterTests.cpp
+++ Vic2ToHoI4Tests/Vic2WorldTests/Map/Vic2ProvinceDefinitionImporterTests.cpp
@@ -6,13 +6,13 @@
 
 TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, ExceptionThrownForMissingDefinitions)
 {
-	EXPECT_THROW(Vic2::importProvinceDefinitions("", {}), std::runtime_error);
+	EXPECT_THROW(Vic2::importProvinceDefinitions("badfolder", {}), std::runtime_error);
 }
 
 
 TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, NoProvinceFromUndefinedColor)
 {
-	const auto definitions = Vic2::importProvinceDefinitions("V2World", {});
+	const auto definitions = Vic2::importProvinceDefinitions("ProvinceDefinition", {});
 
 	const auto province = definitions.getProvinceFromColor(commonItems::Color(std::array{255, 255, 255}));
 	EXPECT_FALSE(province);
@@ -21,9 +21,9 @@ TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, NoProvinceFromUndefinedC
 
 TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, ProvinceFromDefinedColor)
 {
-	const auto definitions = Vic2::importProvinceDefinitions("V2World", {});
+	const auto definitions = Vic2::importProvinceDefinitions("ProvinceDefinition", {});
 
-	const auto province = definitions.getProvinceFromColor(commonItems::Color(std::array{136, 0, 21}));
+	const auto province = definitions.getProvinceFromColor(commonItems::Color(std::array<int, 3>{136, 0, 21}));
 	ASSERT_TRUE(province);
 	EXPECT_EQ(*province, 1);
 }
@@ -31,8 +31,8 @@ TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, ProvinceFromDefinedColor
 
 TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, LandProvincesCanBeDetermined)
 {
-	const auto definitions =
-		 Vic2::importProvinceDefinitions("V2World", {{1, Vic2::Province::Builder{}.setNumber(1).setIsLand().build()}});
+	const auto definitions = Vic2::importProvinceDefinitions("ProvinceDefinition",
+		 {{1, Vic2::Province::Builder{}.setNumber(1).setIsLand().build()}});
 
 	EXPECT_TRUE(definitions.isLandProvince(1));
 	EXPECT_FALSE(definitions.isSeaProvince(1));
@@ -42,7 +42,7 @@ TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, LandProvincesCanBeDeterm
 TEST(Vic2World_Map_Vic2ProvinceDefinitionImporterTests, SeaProvincesCanBeDetermined)
 {
 	const auto definitions =
-		 Vic2::importProvinceDefinitions("V2World", {{1, Vic2::Province::Builder{}.setNumber(1).build()}});
+		 Vic2::importProvinceDefinitions("ProvinceDefinition", {{1, Vic2::Province::Builder{}.setNumber(1).build()}});
 
 	EXPECT_FALSE(definitions.isLandProvince(1));
 	EXPECT_TRUE(definitions.isSeaProvince(1));
