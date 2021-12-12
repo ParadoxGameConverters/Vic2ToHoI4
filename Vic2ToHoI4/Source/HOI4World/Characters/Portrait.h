06a76ae7a1966b47e29bd6c6aba5a64629f29037
Idhrendur <idhrendur@hotmail.com>
2021-12-12 11:31:12 -0800

    Add a class to hold a character portrait

---
+++ Vic2ToHoI4/Source/HOI4World/Characters/Portrait.h
@@ -0,0 +1,36 @@
+#ifndef HOI4_CHARACTERS_PORTRAIT_H
+#define HOI4_CHARACTERS_PORTRAIT_H
+
+
+
+#include <string>
+#include <string_view>
+
+
+
+namespace HoI4
+{
+
+class Portrait
+{
+  public:
+	Portrait(std::string_view type, std::string_view size, std::string_view definition):
+		 type_(type), size_(size), definition_(definition)
+	{
+	}
+
+	[[nodiscard]] const auto& getType() const { return type_; }
+	[[nodiscard]] const auto& getSize() const { return size_; }
+	[[nodiscard]] const auto& getDefinition() const { return definition_; }
+
+  private:
+	std::string type_;
+	std::string size_;
+	std::string definition_;
+};
+
+} // namespace
+
+
+
+#endif // HOI4_CHARACTERS_PORTRAIT_H
