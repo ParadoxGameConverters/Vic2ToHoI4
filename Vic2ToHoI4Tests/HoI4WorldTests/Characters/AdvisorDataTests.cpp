a92aab586956773b837cd63d4a42cc381f528597
Idhrendur <idhrendur@hotmail.com>
2022-01-03 14:16:21 -0800

    Output advisor data

--- Vic2ToHoI4Tests/HoI4WorldTests/Characters/AdvisorDataTests.cpp
+++ Vic2ToHoI4Tests/HoI4WorldTests/Characters/AdvisorDataTests.cpp
@@ -149,7 +149,7 @@ TEST(HoI4World_Characters_AdvisorData, TraitsCanBeSet)
 }
 
 
- TEST(HoI4World_Characters_AdvisorData, ImportedItemsAreDefaulted)
+TEST(HoI4World_Characters_AdvisorData, ImportedItemsAreDefaulted)
 {
 	std::stringstream input;
 
@@ -169,7 +169,7 @@ TEST(HoI4World_Characters_AdvisorData, TraitsCanBeSet)
 }
 
 
- TEST(HoI4World_Characters_AdvisorData, ImportedItemsCanBeSet)
+TEST(HoI4World_Characters_AdvisorData, ImportedItemsCanBeSet)
 {
 	std::stringstream input;
 	input << "= {\n";
@@ -196,7 +196,10 @@ TEST(HoI4World_Characters_AdvisorData, TraitsCanBeSet)
 	input << "\t\t\tresearch_bonus = {\n";
 	input << "\t\t\t\tnuclear = 0.150\n";
 	input << "\t\t\t}\n";
-	input << "\t\t\ttraits = { nuclear_scientist trait_two }\n";
+	input << "\t\t\ttraits = {\n";
+	input << "\t\t\t\tnuclear_scientist\n";
+	input << "\t\t\t\ttrait_two\n";
+	input << "\t\t\t}\n";
 	input << "\t\t\tcost = 100\n";
 	input << "\t\t\tdo_effect = {\n";
 	input << "\t\t\t\tNOT = {\n";
@@ -231,59 +234,59 @@ TEST(HoI4World_Characters_AdvisorData, TraitsCanBeSet)
 	EXPECT_EQ(advisor_data.getLedger(), "army");
 	ASSERT_TRUE(advisor_data.getAvailable().has_value());
 	EXPECT_EQ(*advisor_data.getAvailable(),
-		"{\n"
-		"\t\t\t\tif = {\n"
-		"\t\t\t\t\tlimit = {\n"
-		"\t\t\t\t\t\thas_dlc = \"Man the Guns\"\n"
-		"\t\t\t\t\t}\n"
-		"\t\t\t\t\thas_completed_focus = USA_reach_out_to_the_ware_group\n"
-		"\t\t\t\t}\n"
-		"\t\t\t\tNOT = {\n"
-		"\t\t\t\t\thas_idea = USA_henry_morgenthau\n"
-		"\t\t\t\t}\n"
-		"\t\t\t}");
+		 "{\n"
+		 "\t\t\t\tif = {\n"
+		 "\t\t\t\t\tlimit = {\n"
+		 "\t\t\t\t\t\thas_dlc = \"Man the Guns\"\n"
+		 "\t\t\t\t\t}\n"
+		 "\t\t\t\t\thas_completed_focus = USA_reach_out_to_the_ware_group\n"
+		 "\t\t\t\t}\n"
+		 "\t\t\t\tNOT = {\n"
+		 "\t\t\t\t\thas_idea = USA_henry_morgenthau\n"
+		 "\t\t\t\t}\n"
+		 "\t\t\t}");
 	EXPECT_EQ(advisor_data.getAllowed(),
-		"{\n"
-		"\t\t\t\toriginal_tag = USA\n"
-		"\t\t\t}");
+		 "{\n"
+		 "\t\t\t\toriginal_tag = USA\n"
+		 "\t\t\t}");
 	ASSERT_TRUE(advisor_data.getVisible().has_value());
 	EXPECT_EQ(*advisor_data.getVisible(),
-		"{\n"
-		"\t\t\t\thas_dlc = \"Man the Guns\"\n"
-		"\t\t\t}");
+		 "{\n"
+		 "\t\t\t\thas_dlc = \"Man the Guns\"\n"
+		 "\t\t\t}");
 	ASSERT_TRUE(advisor_data.getResearchBonus().has_value());
 	EXPECT_EQ(*advisor_data.getResearchBonus(),
-		"{\n"
-		"\t\t\t\tnuclear = 0.150\n"
-		"\t\t\t}");
+		 "{\n"
+		 "\t\t\t\tnuclear = 0.150\n"
+		 "\t\t\t}");
 	EXPECT_THAT(advisor_data.getTraits(), testing::ElementsAre("nuclear_scientist", "trait_two"));
 	ASSERT_TRUE(advisor_data.getCost().has_value());
 	EXPECT_EQ(*advisor_data.getCost(), 100.0F);
 	ASSERT_TRUE(advisor_data.getDoEffect().has_value());
 	EXPECT_EQ(*advisor_data.getDoEffect(),
-		"{\n"
-		"\t\t\t\tNOT = {\n"
-		"\t\t\t\t\thas_government = communism\n"
-		"\t\t\t\t}\n"
-		"\t\t\t}");
+		 "{\n"
+		 "\t\t\t\tNOT = {\n"
+		 "\t\t\t\t\thas_government = communism\n"
+		 "\t\t\t\t}\n"
+		 "\t\t\t}");
 	ASSERT_TRUE(advisor_data.getAiWillDo().has_value());
 	EXPECT_EQ(*advisor_data.getAiWillDo(),
-		"{\n"
-		"\t\t\t\tfactor = 10.000\n"
-		"\t\t\t\tmodifier = {\n"
-		"\t\t\t\t\tis_historical_focus_on = yes\n"
-		"\t\t\t\t\thas_game_rule = {\n"
-		"\t\t\t\t\t\trule = USA_ai_behavior\n"
-		"\t\t\t\t\t\toption = default\n"
-		"\t\t\t\t\t}\n"
-		"\t\t\t\t\tfactor = 0.000\n"
-		"\t\t\t\t}\n"
-		"\t\t\t\tmodifier = {\n"
-		"\t\t\t\t\tis_historical_focus_on = yes\n"
-		"\t\t\t\t\tNOT = {\n"
-		"\t\t\t\t\t\thas_dlc = \"Man the Guns\"\n"
-		"\t\t\t\t\t}\n"
-		"\t\t\t\t\tfactor = 0.000\n"
-		"\t\t\t\t}\n"
-		"\t\t\t}");
+		 "{\n"
+		 "\t\t\t\tfactor = 10.000\n"
+		 "\t\t\t\tmodifier = {\n"
+		 "\t\t\t\t\tis_historical_focus_on = yes\n"
+		 "\t\t\t\t\thas_game_rule = {\n"
+		 "\t\t\t\t\t\trule = USA_ai_behavior\n"
+		 "\t\t\t\t\t\toption = default\n"
+		 "\t\t\t\t\t}\n"
+		 "\t\t\t\t\tfactor = 0.000\n"
+		 "\t\t\t\t}\n"
+		 "\t\t\t\tmodifier = {\n"
+		 "\t\t\t\t\tis_historical_focus_on = yes\n"
+		 "\t\t\t\t\tNOT = {\n"
+		 "\t\t\t\t\t\thas_dlc = \"Man the Guns\"\n"
+		 "\t\t\t\t\t}\n"
+		 "\t\t\t\t\tfactor = 0.000\n"
+		 "\t\t\t\t}\n"
+		 "\t\t\t}");
 }
