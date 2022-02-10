0f4b27dc2b5d6b8806ad7a70c00e82a7b8fa0b5c
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-02-10 15:37:50 +0100

    Anscluss acceptance is based on annexer army size - Fixes #1427

--- Vic2ToHoI4/Source/HOI4World/Events/Events.cpp
+++ Vic2ToHoI4/Source/HOI4World/Events/Events.cpp
@@ -193,11 +193,11 @@ void HoI4::Events::createAnnexEvent(const Country& annexer, const Country& annex
 	acceptAiChance += "\t\t\t}\n";
 	acceptAiChance += "\t\t\tmodifier = {\n";
 	acceptAiChance += "\t\t\t\tadd = -15\n";
-	acceptAiChance += "\t\t\t\t" + annexed.getTag() + " = { has_army_size = { size > 39 } }\n";
+	acceptAiChance += "\t\t\t\t" + annexer.getTag() + " = { has_army_size = { size < 40 } }\n";
 	acceptAiChance += "\t\t\t}\n";
 	acceptAiChance += "\t\t\tmodifier = {\n";
 	acceptAiChance += "\t\t\t\tadd = 45\n";
-	acceptAiChance += "\t\t\t\t" + annexed.getTag() + " = { has_army_size = { size < 40 } }\n";
+	acceptAiChance += "\t\t\t\t" + annexer.getTag() + " = { has_army_size = { size > 39 } }\n";
 	acceptAiChance += "\t\t\t}\n";
 	acceptAiChance += "\t\t}";
 	acceptOption.giveAiChance(std::move(acceptAiChance));
@@ -221,11 +221,11 @@ void HoI4::Events::createAnnexEvent(const Country& annexer, const Country& annex
 	refuseAiChance += "\t\t\tbase = 10\n";
 	refuseAiChance += "\t\t\tmodifier = {\n";
 	refuseAiChance += "\t\t\t\tfactor = 0\n";
-	refuseAiChance += "\t\t\t\t" + annexed.getTag() + " = { has_army_size = { size < 30 } }\n";
+	refuseAiChance += "\t\t\t\t" + annexer.getTag() + " = { has_army_size = { size > 39 } }\n";
 	refuseAiChance += "\t\t\t}\n";
 	refuseAiChance += "\t\t\tmodifier = {\n";
 	refuseAiChance += "\t\t\t\tadd = 20\n";
-	refuseAiChance += "\t\t\t\t" + annexed.getTag() + " = { has_army_size = { size > 39 } }\n";
+	refuseAiChance += "\t\t\t\t" + annexer.getTag() + " = { has_army_size = { size < 30 } }\n";
 	refuseAiChance += "\t\t\t}\n";
 	refuseAiChance += "\t\t}";
 	refuseOption.giveAiChance(std::move(refuseAiChance));
