c6d20638f7a0a53aae901fd11faa5ebf9be1ada7
Idhrendur <idhrendur@hotmail.com>
2022-03-21 20:33:04 -0700

    Add a script to report image sizes

---
+++ Resources/scripts/leader_portrait_sizes.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from PIL import Image
+
+original_stdout = sys.stdout
+output_file = open('image_sizes.txt', 'w')
+sys.stdout = output_file
+
+path = '../../Vic2ToHoI4/Data_Files/blankMod/output/gfx/leaders'
+print('Path is ' + path)
+
+print('Gathering files')
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.tga' in file:
+            files.append(os.path.join(r, file))
+        if '.dds' in file:
+            files.append(os.path.join(r, file))
+        if '.png' in file:
+            files.append(os.path.join(r, file))
+
+print('Found ' + str(len(files)) + ' files')
+for f in files:
+    if '.dds' in f:
+        file = open(f,"rb")
+        bytes = list(file.read(20))
+        width = bytes[16] + 256 * bytes[17]
+        height = bytes[12] + 256 * bytes[13]
+        print(f + ' - ' + str(width) + ' x ' + str(height))
+    else:
+        image = Image.open(f)
+        width, height = image.size
+        print(f + ' - ' + str(width) + ' x ' + str(height))
+        
+sys.stdout = original_stdout
