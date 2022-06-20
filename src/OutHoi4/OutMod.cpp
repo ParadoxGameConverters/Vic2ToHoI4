a8d1d71031a7b2e42928dc1ccef3b91aa0432e94
Idhrendur <idhrendur@hotmail.com>
2022-06-20 16:40:14 -0700

    Use replace-path more (#1480)

--- src/OutHoi4/OutMod.cpp
+++ src/OutHoi4/OutMod.cpp
@@ -7,8 +7,67 @@
 
 
 
-void createOutputFolder(const std::string& outputName);
-void createModFiles(const std::string& outputName);
+namespace
+{
+
+void CreateOutputFolder(const std::string& output_name)
+{
+	Log(LogLevel::Info) << "\tCopying blank mod";
+	if (!commonItems::TryCreateFolder("output"))
+	{
+		throw std::runtime_error("Could not create output folder");
+	}
+	if (!commonItems::CopyFolder("blankmod", "output/" + output_name))
+	{
+		throw std::runtime_error("Could not copy blankMod");
+	}
+}
+
+
+void CreateModFiles(const std::string& output_name)
+{
+	Log(LogLevel::Info) << "\tCreating .mod files";
+
+	std::ofstream mod_file("output/" + output_name + ".mod");
+	if (!mod_file.is_open())
+	{
+		throw std::runtime_error("Could not create .mod file");
+	}
+	mod_file << "name = \"Converted - " << output_name << "\"\n";
+	mod_file << "path = \"mod/" << output_name << "/\"\n";
+	mod_file << "user_dir = \"" << output_name << "_user_dir\"\n";
+	mod_file << "replace_path=\"common/countries\"\n";
+	mod_file << "replace_path=\"common/ideologies\"\n";
+	mod_file << "replace_path=\"common/national_focus\"\n";
+	mod_file << "replace_path=\"events\"\n";
+	mod_file << "replace_path=\"history/countries\"\n";
+	mod_file << "replace_path=\"history/states\"\n";
+	mod_file << "replace_path=\"history/units\"\n";
+	mod_file << "replace_path=\"map/supplyareas\"\n";
+	mod_file << "replace_path=\"map/strategicregions\"\n";
+	mod_file << "supported_version=\"1.11.*\"";
+	mod_file.close();
+
+	std::ofstream descriptor_file("output/" + output_name + "/descriptor.mod");
+	if (!descriptor_file.is_open())
+	{
+		throw std::runtime_error("Could not create descriptor.mod");
+	}
+	descriptor_file << "name = \"Converted - " << output_name << "\"\n";
+	descriptor_file << "replace_path=\"common/countries\"\n";
+	descriptor_file << "replace_path=\"common/ideologies\"\n";
+	descriptor_file << "replace_path=\"common/national_focus\"\n";
+	descriptor_file << "replace_path=\"events\"\n";
+	descriptor_file << "replace_path=\"history/countries\"\n";
+	descriptor_file << "replace_path=\"history/states\"\n";
+	descriptor_file << "replace_path=\"history/units\"\n";
+	descriptor_file << "replace_path=\"map/supplyareas\"\n";
+	descriptor_file << "replace_path=\"map/strategicregions\"\n";
+	descriptor_file << "supported_version=\"1.11.*\"";
+	descriptor_file.close();
+}
+
+} // namespace
 
 
 
@@ -36,56 +95,10 @@ void output(const HoI4::World& destWorld,
 	Log(LogLevel::Progress) << "80%";
 	Log(LogLevel::Info) << "Outputting mod";
 
-	createOutputFolder(outputName);
-	createModFiles(outputName);
+	CreateOutputFolder(outputName);
+	CreateModFiles(outputName);
 	Log(LogLevel::Progress) << "85%";
 	copyFlags(destWorld.getCountries(), outputName, vic2Mods, destWorld.getMajorIdeologies());
 	Log(LogLevel::Progress) << "90%";
 	OutputWorld(destWorld, outputName, debugEnabled, theConfiguration);
-}
-
-
-void createOutputFolder(const std::string& outputName)
-{
-	Log(LogLevel::Info) << "\tCopying blank mod";
-	if (!commonItems::TryCreateFolder("output"))
-	{
-		throw std::runtime_error("Could not create output folder");
-	}
-	if (!commonItems::CopyFolder("blankmod", "output/" + outputName))
-	{
-		throw std::runtime_error("Could not copy blankMod");
-	}
-}
-
-
-void createModFiles(const std::string& outputName)
-{
-	Log(LogLevel::Info) << "\tCreating .mod files";
-
-	std::ofstream modFile("output/" + outputName + ".mod");
-	if (!modFile.is_open())
-	{
-		throw std::runtime_error("Could not create .mod file");
-	}
-	modFile << "name = \"Converted - " << outputName << "\"\n";
-	modFile << "path = \"mod/" << outputName << "/\"\n";
-	modFile << "user_dir = \"" << outputName << "_user_dir\"\n";
-	modFile << "replace_path=\"common/ideologies\"\n";
-	modFile << "replace_path=\"history/countries\"\n";
-	modFile << "replace_path=\"history/states\"\n";
-	modFile << "supported_version=\"1.11.*\"";
-	modFile.close();
-
-	std::ofstream descriptorFile("output/" + outputName + "/descriptor.mod");
-	if (!descriptorFile.is_open())
-	{
-		throw std::runtime_error("Could not create descriptor.mod");
-	}
-	descriptorFile << "name = \"Converted - " << outputName << "\"\n";
-	descriptorFile << "replace_path=\"common/ideologies\"\n";
-	descriptorFile << "replace_path=\"history/countries\"\n";
-	descriptorFile << "replace_path=\"history/states\"\n";
-	descriptorFile << "supported_version=\"1.11.*\"";
-	descriptorFile.close();
 }
