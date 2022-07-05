330cc41d347632e40501e9fc09ce22f9c40f4e12
Idhrendur <idhrendur@hotmail.com>
2022-07-04 17:00:30 -0700

    Make possible path use new style names

--- src/HOI4World/Map/PossiblePath.h
+++ src/HOI4World/Map/PossiblePath.h
@@ -13,25 +13,25 @@ namespace HoI4
 class PossiblePath
 {
   public:
-	explicit PossiblePath(int initialProvince) { provinces_.push_back(initialProvince); }
+	explicit PossiblePath(int initial_province) { provinces_.push_back(initial_province); }
 
-	void AddProvince(int province, double additionalCost)
+	void AddProvince(int province, double additional_cost)
 	{
 		provinces_.push_back(province);
-		cost_ += additionalCost;
+		cost_ += additional_cost;
 	}
 	void ReplaceProvinces(std::vector<int> new_provinces) { provinces_ = std::move(new_provinces); }
 	void SetLevel(int level) { level_ = level; }
 
-	// higher is prioritized where we want lower to be, so reverse the meaning of less than
-	[[nodiscard]] bool operator<(const PossiblePath& rhs) const { return cost_ > rhs.cost_; }
 	[[nodiscard]] int GetLastProvince() const { return provinces_[provinces_.size() - 1]; }
 	[[nodiscard]] int GetFirstProvince() const { return provinces_[0]; }
 	[[nodiscard]] std::vector<int> GetProvinces() const { return provinces_; }
 	[[nodiscard]] int GetLevel() const { return level_; }
 	[[nodiscard]] double GetCost() const { return cost_; }
 
-	bool operator==(const PossiblePath&) const = default;
+	// higher is prioritized where we want lower to be in priority queues, so reverse the meaning of <
+	[[nodiscard]] bool operator<(const PossiblePath& rhs) const { return cost_ > rhs.cost_; }
+	[[nodiscard]] bool operator==(const PossiblePath&) const = default;
 
   private:
 	std::vector<int> provinces_;
