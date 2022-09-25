4948b70e9d197146f81801744fb6919c1be4c592
Stephen May <idhrendur@hotmail.com>
2022-09-24 23:11:16 -0700

    Add a script to run clang-tidy

--- tools/build_Vic2ToHoI4_release.sh
+++ tools/build_Vic2ToHoI4_release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake -H. -Bbuild/linux-intermediate && cmake --build ./build/linux-intermediate -- -j40 && cd build/Release-LinuxTests && ./Vic2ToHoi4ConverterTests && cd ..
+cmake -H. -Bbuild/linux-intermediate && cmake --build ./build/linux-intermediate --target Vic2ToHoi4Converter Fronter Vic2ToHoi4ConverterTests -- -j40 && cd build/Release-LinuxTests && ./Vic2ToHoi4ConverterTests && cd ..
