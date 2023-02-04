1d4ef54e7cc5acc6218b84c4930ba434be382408
Ivo Elezović <ivo@element.hr>
2023-02-05 00:07:52 +0100

    RELEASE THE KRAKEN (#1611)
    * RELEASE THE KRAKEN
    
    * wf
    
    * try swapping deps
    
    * maybe like so
    
    * linux pch & declutter
    
    * clang
    
    * gcov
    
    * wf
    
    * cleanup

--- tools/build_Vic2ToHoI4_release.sh
+++ tools/build_Vic2ToHoI4_release.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cmake -H. -Bbuild/linux-intermediate && cmake --build ./build/linux-intermediate --target Vic2ToHoi4Converter Fronter Vic2ToHoi4ConverterTests -- -j40 && cd build/Release-LinuxTests && ./Vic2ToHoi4ConverterTests && cd ..
+cmake -H. -Bbuild/linux-intermediate && 
+cmake --build ./build/linux-intermediate \
+  --target Vic2ToHoi4Converter \
+           ConverterFrontend \
+           Vic2ToHoi4ConverterTests \
+           -- -j40 && 
+cd build/Release-LinuxTests && 
+./Vic2ToHoi4ConverterTests && 
+cd ..
