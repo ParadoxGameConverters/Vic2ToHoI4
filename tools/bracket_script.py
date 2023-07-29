da78ca09ffa03c0d3eddba335f4877c21c7936cb
Idhrendur <idhrendur@hotmail.com>
2023-07-29 15:22:29 -0700

    Update bracket_script.py

--- tools/bracket_script.py
+++ tools/bracket_script.py
@@ -22,10 +22,12 @@ for filename in glob.iglob(root_dir + '**/*.txt', recursive=True):
     if filename == './data/blank_mod/common/ai_strategy/default.txt' or \
     filename == './data/blank_mod/common/country_leader/00_traits.txt' or \
     filename == './data/configurables/converterFocuses.txt contains' or \
+    filename == './data/configurables/Divergences of Darkness_province_mappings.txt' or \
     filename == './data/configurables/Greater Flavor Mod_province_mappings.txt' or \
     filename == './data/configurables/Historical Flavour Mod v1.27I_province_mappings.txt' or \
     filename == './data/configurables/Historical Project Mod 0.4.6_province_mappings.txt' or \
     filename == './data/configurables/province_mappings.txt' or \
+    filename == './data/configurables/TGC - The Grand Combination_province_mappings.txt' or \
     filename == './data/configurables/The Concert of Europe_province_mappings.txt' or \
     filename == './data/test_files/Technology/BaseGameTech2.txt':
         file_encoding = 'cp1252'
