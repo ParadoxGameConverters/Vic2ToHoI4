5d567ae69954fc15e3fddcaea86b497c5d5fd0bd
Idhrendur <idhrendur@hotmail.com>
2023-08-06 16:03:13 -0700

    Review comments.

--- src/V2World/Countries/CommonCountriesDataFactory.cpp
+++ src/V2World/Countries/CommonCountriesDataFactory.cpp
@@ -18,26 +18,26 @@ std::tuple<std::map<std::string, Vic2::CommonCountryData>, std::vector<Vic2::Par
 	std::map<std::string, Vic2::CommonCountryData> common_countries_data;
 	std::vector<Vic2::Party> parties;
 
-	Vic2::CommonCountryData::Factory common_country_data_factory;
-
-	commonItems::parser countries_parser;
-	countries_parser.registerKeyword("dynamic_tags", commonItems::ignoreItem);
-	countries_parser.registerRegex(commonItems::catchallRegex,
-		 [&common_countries_data, &parties, &common_country_data_factory, mod_filesystem](const std::string& tag,
-			  std::istream& input_stream) {
-			 const std::string country_file_name = commonItems::getString(input_stream);
-			 const std::unique_ptr<Vic2::CommonCountryData> country_data =
-				  common_country_data_factory.ImportCommonCountryData(country_file_name, mod_filesystem);
-			 common_countries_data[tag] = *country_data;
-			 for (const Vic2::Party& party: country_data->GetParties())
-			 {
-				 parties.emplace_back(party);
-			 }
-		 });
-
 	if (const std::optional<std::string> possible_file = mod_filesystem.GetActualFileLocation(country_list_file);
 		 possible_file)
 	{
+		Vic2::CommonCountryData::Factory common_country_data_factory;
+
+		commonItems::parser countries_parser;
+		countries_parser.registerKeyword("dynamic_tags", commonItems::ignoreItem);
+		countries_parser.registerRegex(commonItems::catchallRegex,
+			 [&common_countries_data, &parties, &common_country_data_factory, mod_filesystem](const std::string& tag,
+				  std::istream& input_stream) {
+				 const std::string country_file_name = commonItems::getString(input_stream);
+				 const std::unique_ptr<Vic2::CommonCountryData> country_data =
+					  common_country_data_factory.ImportCommonCountryData(country_file_name, mod_filesystem);
+				 common_countries_data[tag] = *country_data;
+				 for (const Vic2::Party& party: country_data->GetParties())
+				 {
+					 parties.emplace_back(party);
+				 }
+			 });
+
 		countries_parser.parseFile(*possible_file);
 	}
 
