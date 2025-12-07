5cbac3b3f049e5dc55f04416d67ee767f45b63e0
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2025-12-07 16:55:15 +0100

    Replace path common/ai_navy/goal

--- src/OutHoi4/OutMod.cpp
+++ src/OutHoi4/OutMod.cpp
@@ -44,6 +44,7 @@ void CreateModFiles(const std::filesystem::path& output_name)
 	mod_file << "path = \"mod/" << output_name.string() << "/\"\n";
 	mod_file << "user_dir = \"" << output_name.string() << "_user_dir\"\n";
 	mod_file << "replace_path=\"common/abilities\"\n";
+	mod_file << "replace_path=\"common/ai_navy/goals\"\n";
 	mod_file << "replace_path=\"common/countries\"\n";
 	mod_file << "replace_path=\"common/ideologies\"\n";
 	mod_file << "replace_path=\"common/military_industrial_organization/organizations\"\n";
@@ -68,6 +69,7 @@ void CreateModFiles(const std::filesystem::path& output_name)
 	}
 	descriptor_file << "name = \"Converted - " << output_name.string() << "\"\n";
 	descriptor_file << "replace_path=\"common/abilities\"\n";
+	descriptor_file << "replace_path=\"common/ai_navy/goals\"\n";
 	descriptor_file << "replace_path=\"common/countries\"\n";
 	descriptor_file << "replace_path=\"common/ideologies\"\n";
 	descriptor_file << "replace_path=\"common/military_industrial_organization/organizations\"\n";
