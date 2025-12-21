176d7b62cfbce9ec967724d4d9f26349418a5c64
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2025-12-21 21:10:40 +0100

    Replace path common/raids

--- src/OutHoi4/OutMod.cpp
+++ src/OutHoi4/OutMod.cpp
@@ -61,6 +61,7 @@ void CreateModFiles(const std::filesystem::path& output_name)
 	mod_file << "replace_path=\"common/on_actions\"\n";
 	mod_file << "replace_path=\"common/peace_conference/ai_peace\"\n";
 	mod_file << "replace_path=\"common/peace_conference/cost_modifiers\"\n";
+	mod_file << "replace_path=\"common/raids\"\n";
 	mod_file << "replace_path=\"events\"\n";
 	mod_file << "replace_path=\"history/countries\"\n";
 	mod_file << "replace_path=\"history/states\"\n";
@@ -96,6 +97,7 @@ void CreateModFiles(const std::filesystem::path& output_name)
 	descriptor_file << "replace_path=\"common/on_actions\"\n";
 	descriptor_file << "replace_path=\"common/peace_conference/ai_peace\"\n";
 	descriptor_file << "replace_path=\"common/peace_conference/cost_modifiers\"\n";
+	descriptor_file << "replace_path=\"common/raids\"\n";
 	descriptor_file << "replace_path=\"events\"\n";
 	descriptor_file << "replace_path=\"history/countries\"\n";
 	descriptor_file << "replace_path=\"history/states\"\n";
