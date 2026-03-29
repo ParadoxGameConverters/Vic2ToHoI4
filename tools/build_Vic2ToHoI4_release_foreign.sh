a4745c53bcdbc8d66ac2a9c58488bc4b4d96dd4c
Idhrendur <idhrendur@hotmail.com>
2026-03-29 16:01:54 -0700

    Remove unneeded scripts

--- tools/build_Vic2ToHoI4_release_foreign.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cmake -H. -Bbuild/linux-intermediate && cmake --build ./build/linux-intermediate -- -j5 && cd build/Release-LinuxTests && ./Vic2ToHoi4ConverterTests && cd ..
--- tools/build_fronter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# to be run from the project root directory
-cd external/Fronter
-./build_linux.sh
-cp -r Release/* ../../build/Release-Linux
